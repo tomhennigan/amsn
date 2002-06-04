0943f318a339a39ab2b1dac769a8232a36712f92
airadier <airadier@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2002-06-04 14:34:15 +0000

    S
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@25 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/config.tcl
@@ -0,0 +1,69 @@
+set config(login) ""			;# These are defaults for users without
+set config(save_password) 0		;# a config file
+set config(keep_logs) 0
+set config(proxy) ""
+set config(start_ns_server) "messenger.hotmail.com:1863"
+set config(last_client_version) ""
+#by AIM
+set config(sound) 1
+set config(browser) "mozilla"
+set config(soundcommand) "esdplay"
+set config(language) "en"
+set config(adverts) 0
+set config(autohotlogin) 1
+set config(showonline) 1
+set config(showoffline) 1
+#end AIM
+
+set password ""
+
+
+proc save_config {} {
+   global tcl_platform config HOME version password
+
+   if {$tcl_platform(platform) == "unix"} {
+      set file_id [open "${HOME}/config" w 00600]
+   } else {
+      set file_id [open "${HOME}/config" w]
+   }
+   puts $file_id "amsn_config_version 1"
+   set config(last_client_version) $version
+
+   set config_entries [array get config]
+   set items [llength $config_entries]
+   for {set idx 0} {$idx < $items} {incr idx 1} {
+      set var_attribute [lindex $config_entries $idx]; incr idx 1
+      set var_value [lindex $config_entries $idx]
+      puts $file_id "$var_attribute $var_value"
+   }
+   if {$config(save_password)} {
+      puts $file_id "password ${password}"
+   }
+   close $file_id
+}
+
+proc load_config {} {
+   global config HOME password
+
+   if {([file readable "${HOME}/config"] == 0) ||
+       ([file isfile "${HOME}/config"] == 0)} {
+      return 1
+   }
+   set file_id [open "${HOME}/config" r]
+   gets $file_id tmp_data
+   if {$tmp_data != "amsn_config_version 1"} {	;# config version not supported!
+      return 1
+   }
+   while {[gets $file_id tmp_data] != "-1"} {
+      set var_data [split $tmp_data]
+      set var_attribute [lindex $var_data 0]
+      set var_value [lindex $var_data 1]
+      set config($var_attribute) $var_value
+   }
+   if {[info exists config(password)]} {
+      set password $config(password)
+      unset config(password)
+   }
+   close $file_id
+}
+
---
+++ amsn/smileys.tcl
@@ -0,0 +1,19 @@
+set emotions {{":-)" smile} {":)" smile} {":-D" smiled} {":D" smiled}
+	{":->" smiled} {":>" smiled} {":-O" smileo} {":O" smileo} {":-P" smilep}
+	{":P" smilep} {";-)" wink} {";)" wink} {":-(" sad} {":(" sad}
+	{":-<" sad} {":<" sad} {":-S" crooked} {":S" crooked} {":-|" disgust}
+	{":|" disgust} {"(Y)" thumbu} {"(N)" thumbd} {"(L)" love} {"(U)" unlove}
+	{"(K)" lips} {"(G)" gift} {"(F)" rose} {"(X)" emgirl} {"(Z)" emboy}
+	{"(P)" photo} {"(B)" beer} {"(D)" coctail} {"(T)" emphone} {"(@)" emcat}
+	{"(C)" emcup} {"(I)" embulb} {"(H)" emhottie} {"(S)" emsleep}
+	{"(*)" emstar} {"(8)" emnote} {"(E)" email} {"(M)" messenger}
+	{":-[" vampire} {":[" vampire} {"(\})" girlhug} {"(\{)" boyhug}
+	{"(A)" angel} {"(6)" devil} {"(^)" cake} {"(O)" clk} {":-@" angry}
+	{":@" angry} {"(&)" dog} {"(W)" rosew} {":`(" smilec} {":$" smilemb} 
+	{":-$" smilemb} {"(#)" sun} {"(R)" rainbow}}
+
+set emotion_files {smile smiled smileo smilep wink sad crooked disgust thumbu
+	thumbd love unlove lips gift rose emgirl emboy photo beer coctail
+	emphone emcat emcup embulb emhottie emsleep emstar emnote email
+	messenger vampire girlhug boyhug angel devil cake clk angry dog rosew
+	smilec smilemb smilemb sun rainbow}
