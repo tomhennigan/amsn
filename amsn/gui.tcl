354be0b62b1e3c74c9053bb48678765be8aece33
airadier <airadier@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2002-06-17 09:51:47 +0000

    GUI namespace module
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@111 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/gui.tcl
@@ -0,0 +1,37 @@
+
+namespace eval ::amsn {
+   namespace export fileTransfer fileTransferProgress errorMsg
+   
+   proc errorMsg { msg } {
+     msg_box msg
+   }
+   
+   proc fileTransfer {filename filesize cookie sb_name} {
+      global files_dir
+      set answer [tk_messageBox -message "[trans acceptfile $filename $filesize $files_dir]" -type yesno -icon question]
+      if {$answer == "yes"} {
+         ::MSN::acceptFT $cookie $sb_name
+      } else {
+         ::MSN::rejectFT $cookie
+      }
+   }
+   
+   proc fileTransferProgress {mode sbn bytes filesize} {
+      set win_name "msg_[string tolower ${sbn}]"
+			
+      #Windows closed?
+      .${win_name}.status configure -state normal
+      .${win_name}.status delete 0.0 end
+      
+      if { $bytes == $filesize } {
+        .${win_name}.status insert end "[trans filetransfercomplete]\n"         
+      }
+      
+      if { $mode == "r" } {
+        .${win_name}.status insert end "[trans receivedbytes $bytes $filesize].\n"
+      } else {
+        .${win_name}.status insert end "[trans sentbytes $bytes $filesize].\n"
+      }
+      .${win_name}.status configure -state disabled
+   }
+}
