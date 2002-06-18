ba5a83b8879cad232d9a5458ee3ba14e4584ac62
lordofscripts <lordofscripts@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2002-06-18 14:28:12 +0000

    Implemented dialog for address book. Namespace abookGui
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@123 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/abook.tcl
+++ amsn/abook.tcl
@@ -3,13 +3,14 @@
 # $Id$
 #=======================================================================
 namespace eval ::abook {
-   namespace export setGroup getGroup setContact
+   namespace export setGroup getGroup setContact getContact
 
    #
    # P R I V A T E
    #
    variable contacts;	# Array for contacts
    			# contacts(email) {		   BPR*
+			#	handle			   LST
 			#       groupId			   LST
 			#	phone { home work mobile } BPR.PHH/PHW/PHM
 			#	mobile			   BPR.MOB
@@ -38,8 +39,11 @@ namespace eval ::abook {
        variable groupCnt
        variable groups
 
-       if { $nr > $groupCnt } { return "" }
-       else { return $groups($nr) }
+       if { $nr > $groupCnt } { 
+       	   return "" 
+       } else { 
+           return $groups($nr) 
+       }
    }
 
    proc setContact { email field value } {
@@ -47,37 +51,157 @@ namespace eval ::abook {
 
 	# Phone numbers are countryCode%20areaCode%20subscriberNumber
 	if { ![info exists contacts($email)] } {
-	  set contacts($email) [list Dummy Dummy Dummy Dummy Dummy]
+	  set contacts($email) [list Dummy Dummy Dummy Dummy Dummy Dummy]
 	}
         switch $field {
 	    FL {	;# From LST.FL command, contains email, groupId
-		set contacts($email) [list  $value "" "" "" "" ]
+		set contacts($email) [list  $value "" "" "" "" "" ]
 #		puts "ABOOK: creating $email GroupId $value"
 #		puts "size [llength $contacts($email)]"
 	    }
-	    PHH {	;# From BPR.PHH (Home Phone Number)
+	    nick {	;# From LST.FL (User handle)
 		set contacts($email) [lreplace $contacts($email) 1 1 $value]
 #		puts "ABOOK: $email Home $value"
 	    }
-	    PHW {	;# From BPR.PHW (Work Phone Number)
+	    PHH {	;# From BPR.PHH (Home Phone Number)
 		set contacts($email) [lreplace $contacts($email) 2 2 $value]
+#		puts "ABOOK: $email Home $value"
+	    }
+	    PHW {	;# From BPR.PHW (Work Phone Number)
+		set contacts($email) [lreplace $contacts($email) 3 3 $value]
 #		puts "ABOOK: $email Work $value"
 	    }
 	    PHM {	;# From BPR.PHM (Mobile Phone Number)
-		set contacts($email) [lreplace $contacts($email) 3 3 $value]
+		set contacts($email) [lreplace $contacts($email) 4 4 $value]
 #		puts "ABOOK: $email Mobile $value"
 	    }
-	    MOB {	;# From BPR.MOB (Mobile settings) Y|N
-		set contacts($email) [lreplace $contacts($email) 4 4 $value]
+	    MOB {	;# From BPR.MOB (Mobile pager) Y|N
+		set contacts($email) [lreplace $contacts($email) 5 5 $value]
 #		puts "ABOOK: $email MobileSet $value"
 	    }
 	    default {
 	        puts "setContact unknown field $field -> $value"
 	    }
 	}
+#puts "$field -> $contacts($email)"
+   }
+
+   proc getContact { email cdata } {
+   	variable contacts 
+	upvar $cdata data
+
+	if { ![info exists contacts($email)] } {
+	    puts "ERROR: unknown contact!"
+	    return
+	}
+	
+#puts $contacts($email)
+	set groupName    [::abook::getGroup [lindex $contacts($email) 0]]
+	set data(group)  [urldecode $groupName]
+	set data(handle) [urldecode [lindex $contacts($email) 1]]
+	set data(phh)    [urldecode [lindex $contacts($email) 2]]
+	set data(phw)    [urldecode [lindex $contacts($email) 3]]
+	set data(phm)    [urldecode [lindex $contacts($email) 4]]
+	set data(mob)    [urldecode [lindex $contacts($email) 5]]
+	set data(available) Y
+   }
+}
+
+namespace eval ::abookGui {
+   namespace export showEntry
+
+   #
+   # P R I V A T E
+   #
+
+   #
+   # P R O T E C T E D
+   #
+
+   #
+   # P U B L I C
+   #
+   proc showEntry { email } {
+	set cd(available) N
+	::abook::getContact $email cd
+
+	if { $cd(available) == "N" } {
+	    msg_box "No data available yet"
+	    return
+	}
+
+	# Generate a unique (almost) toplevel so that we can
+	# have several open info windows (for different users)
+	set w [string tolower $email]
+	set w [string trim $w]
+	set ends  [string first @ $w]
+	incr ends -1
+	set w [string range $w 0 $ends]
+	set w [string map { @ "" _ "" - "" . "" } $w]
+ 	set w ".a$w"	
+
+	if [winfo exists $w] {
+	    return 
+	}
+
+	toplevel $w
+	wm title $w "[trans about] $email"
+	wm geometry $w 185x140
+	set nbtIdent "[trans identity]"
+	set nbtPhone "[trans phone]"
+	frame $w.n
+	    pack [notebook $w.n.p $nbtIdent $nbtPhone] \
+	    	-expand 1 -fill both -padx 1m -pady 1m
+	#  .----------.
+	# _| Identity |________________________________________________
+	set nbIdent [getNote $w.n.p $nbtIdent]
+   	label $nbIdent.e -text "Email:" -font bboldf
+   	label $nbIdent.e1 -text $email -font splainf
+	label $nbIdent.h -text "[trans handle]:" -font bboldf
+	label $nbIdent.h1 -text $cd(handle) -font splainf
+	label $nbIdent.g -text "[trans group]:" -font bboldf
+	label $nbIdent.g1 -text $cd(group) -font splainf
+	grid $nbIdent.e -row 0 -column 0 -sticky e
+	grid $nbIdent.e1 -row 0 -column 1
+	grid $nbIdent.h -row 1 -column 0 -sticky e
+	grid $nbIdent.h1 -row 1 -column 1
+	grid $nbIdent.g -row 2 -column 0 -sticky e
+	grid $nbIdent.g1 -row 2 -column 1
+        bind $w <Control-i> "pickNote $w.n.p $nbtIdent"
+
+	#  .--------.
+	# _| Phones |________________________________________________
+	set nbPhone [getNote $w.n.p $nbtPhone]
+	label $nbPhone.h -font bboldf -text "[trans home]:"
+	label $nbPhone.h1 -font splainf -text $cd(phh)
+	label $nbPhone.w -font bboldf -text "[trans work]:"
+	label $nbPhone.w1 -font splainf -text $cd(phw)
+	label $nbPhone.m -font bboldf -text "[trans mobile]:"
+	label $nbPhone.m1 -font splainf -text $cd(phm)
+	label $nbPhone.p -font bboldf -text "[trans pager]:"
+	label $nbPhone.p1 -font splainf -text $cd(mob)
+	grid $nbPhone.h -row 0 -column 0 -sticky e
+	grid $nbPhone.h1 -row 0 -column 1
+	grid $nbPhone.w -row 1 -column 0 -sticky e
+	grid $nbPhone.w1 -row 1 -column 1
+	grid $nbPhone.m -row 2 -column 0 -sticky e
+	grid $nbPhone.m1 -row 2 -column 1
+	grid $nbPhone.p -row 3 -column 0 -sticky e
+	grid $nbPhone.p1 -row 3 -column 1
+        bind $w <Control-p> "pickNote $w.n.p $nbtPhone"
+
+	frame $w.b
+	    button $w.b.ok -text "[trans close]" -command "destroy $w"
+	    pack $w.b.ok
+
+	pack $w.n $w.b -side top
+	bind $w <Control-c> { destroy $w }
    }
 }
 # $Log$
+# Revision 1.4  2002/06/18 14:28:12  lordofscripts
+# Implemented dialog for address book. Namespace abookGui
+#
 # Revision 1.3  2002/06/17 12:31:32  airadier
 # Quick hack to avoid a bug when adding contacts (var contacts($email) non existing)
 #
