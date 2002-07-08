c6e5761b2762b22c36688cee837783c73f767c0c
lordofscripts <lordofscripts@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2002-07-08 00:06:41 +0000

    - Added LoginData to set proxy authentication parameters (when needed) - Added Setup which is used during the setup phase of proxy connection - Added experiemental SOCKS5 support (requires socks.tcl)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@342 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/proxy.tcl
+++ amsn/proxy.tcl
@@ -3,14 +3,18 @@
 #	This file defines various procedures which implement a
 #	Proxy access system. Code originally by Dave Mifsud,
 #	converted to namespace and improved by D. Emilio Grimaldo T.
+#	SOCKS5 support (integration) is experimental!!!
 #
 # RCS: @(#) $Id$
 
 package provide Proxy 0.1
 package require http  2.3
 
+# This should be converted to a proper package, to use with package require
+source [file join $program_dir socks.tcl]	;# SOCKS5 proxy support
+
 namespace eval ::Proxy {
-namespace export Init Connect Read OnCallback
+namespace export Init LoginData Setup Connect Read OnCallback
 
 variable proxy_host "";
 variable proxy_port "";
@@ -36,6 +40,7 @@ proc Init { proxy ptype } {
 
   # TODO Test with Proxy
   # for the moment, just configure the http to use proxy or not
+  set proxy_type $ptype
   if {($proxy != ":") && ($proxy != "") } {
      set lproxy [split $proxy ":"]
      set proxy_host [lindex $lproxy 0]
@@ -46,15 +51,71 @@ proc Init { proxy ptype } {
      set proxy_port ""
      ::http::config -proxyhost ""
   }
-}
+};# Proxy::Init
+
+proc LoginData { authenticate user passwd} {
+    variable proxy_with_authentication
+    variable proxy_username
+    variable proxy_password
+
+    set proxy_with_authentication $authenticate
+    set proxy_username $user
+    set proxy_password $passwd
+};# Proxy::LoginData
+
+# ::Proxy::Setup next readable_handler $socket_name
+proc Setup {next_handler readable_handler name} {
+    variable proxy_type
+    upvar $next_handler next
+    upvar $readable_handler read
+
+    switch $proxy_type {
+        http {
+	    set next_handler "::Proxy::Connect $name"
+	    set read "::Proxy::Read $name"
+	}
+	socks5 {
+	    set next_handler "::Proxy::Connect $name"
+	    set read "socks::readable $name"
+	}
+    }
+    status_log "Proxy::Setup $proxy_type $name"
+};# Proxy::Setup
 
 proc Connect {name} {
+   variable proxy_type
+   variable proxy_username
+   variable proxy_password
+   variable proxy_with_authentication
+   
    fileevent [sb get $name sock] writable {}
    sb set $name stat "pc"
-   set tmp_data "CONNECT [join [sb get $name serv] ":"] HTTP/1.0"
-   status_log "PROXY SEND: $tmp_data\n"
-   puts -nonewline [sb get $name sock] "$tmp_data\r\n\r\n"
-}
+   set remote_server [sb get $name serv]
+   set remote_port 80
+   switch $proxy_type {
+       http {
+	   set tmp_data "CONNECT [join $remote_server ":"] HTTP/1.0"
+	   status_log "PROXY SEND: $tmp_data\n"
+	   puts -nonewline [sb get $name sock] "$tmp_data\r\n\r\n"
+       }
+       socks5 {
+	   set pusername $proxy_username
+	   set ppassword $proxy_password
+   	   if {$proxy_with_authentication == 0} {
+	       set pusername ""
+	       set ppassword ""
+	   }
+   	   set pstat [socks:init $name $remote_server $remote_port $proxy_with_authentication $pusername $ppassword]
+	   if {$pstat != "OK"} {
+	        status_log "SOCKS5: $pstat"
+		# DEGT Use a callback mechanism to keep code pure
+		if {$proxy_dropped_cb != ""} {
+		    set cbret [eval $proxy_dropped_cb "dropped" "$name"]
+		}
+	   }
+       }
+   }; #proxy type
+};# Proxy::Connect
 
 proc Read { name } {
    variable proxy_header
@@ -89,10 +150,15 @@ proc Read { name } {
 	  }
    }
    return 0
-}
+};# Proxy::Read
 }
 ###################################################################
 # $Log$
+# Revision 1.2  2002/07/08 00:06:41  lordofscripts
+# - Added LoginData to set proxy authentication parameters (when needed)
+# - Added Setup which is used during the setup phase of proxy connection
+# - Added experiemental SOCKS5 support (requires socks.tcl)
+#
 # Revision 1.1  2002/07/07 03:08:29  lordofscripts
 # - T-PROXY This namespace created based on HTTP proxy code by Dave Mifsud.
 # - Enhanced by using callback mechanism to make it generic
