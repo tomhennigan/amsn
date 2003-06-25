1d38e764cdcdaf9776590e8a8e9552efeae95237
airadier <airadier@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2003-06-25 12:18:22 +0000

    Changed smileys into lowercase
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@990 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/smileys.tcl
+++ amsn/smileys.tcl
@@ -8,22 +8,22 @@ set emotions {
 	{":-S" crooked} {":S" crooked} {":-O" smileo} {":O" smileo} {":'(" smilec} {":`(" smilec2} 
 	{":@" angry}  {":-@" angry} {":->" smiled} {"8o|" aggresive} {":$" smilemb} {":-$" smilemb} {":-#" zip} 
 	{":-*" kiss} {":^)" new7} {"8-|" glasses} {"8-)" new5} {"|-)" new4} {"+o(" new3}
-	{"*-)" new2} {"^o)" new1} {"(H)" emhottie} {"(BRB)" brb} {"<:o)" party} {"(A)" angel}  {"(6)" devil}
-	{"(L)" love} {"(U)" unlove} {"(K)" lips} {"(F)" rose} {"(W)" rosew} 
+	{"*-)" new2} {"^o)" new1} {"(h)" emhottie} {"(brb)" brb} {"<:o)" party} {"(a)" angel}  {"(6)" devil}
+	{"(l)" love} {"(u)" unlove} {"(k)" lips} {"(F)" rose} {"(W)" rosew} 
 	
 	
-	{"(B)" beer} {"(D)" coctail} {"(C)" emcup} {"(^)" cake} {"(PL)" toilet} {"(PI)" pizza}
+	{"(b)" beer} {"(D)" coctail} {"(C)" emcup} {"(^)" cake} {"(pl)" toilet} {"(pi)" pizza}
 	
-	{"(T)" emphone} {"(mp)" mobphone} {"(E)" email} {"(CO)" computer} {"(O)" clk} {"(P)" photo} {"(~)" film} 
-	{"(I)" embulb} {"(%)" handcuffs} {"(||)" chopsticks} {"(UM)" umbrella} {"(XX)" xbox} {"(MO)" money}
-	{"(G)" gift} {"(CI)" cigarette} {"(AU)" car} {"(AP)" plane} {"(SO)" football} {"(8)" emnote} {"(?)" asl}
+	{"(t)" emphone} {"(mp)" mobphone} {"(e)" email} {"(co)" computer} {"(o)" clk} {"(p)" photo} {"(~)" film} 
+	{"(i)" embulb} {"(%)" handcuffs} {"(||)" chopsticks} {"(um)" umbrella} {"(xx)" xbox} {"(mo)" money}
+	{"(g)" gift} {"(ci)" cigarette} {"(au)" car} {"(ap)" plane} {"(so)" football} {"(8)" emnote} {"(?)" asl}
 	
-	{"(*)" emstar} {"(S)" emsleep} {"(R)" rainbow} {"(ST)" rain} {"(#)" sun} {"(LI)" storm} {"(IP)" island}
+	{"(*)" emstar} {"(s)" emsleep} {"(r)" rainbow} {"(st)" rain} {"(#)" sun} {"(li)" storm} {"(ip)" island}
 	
-	 {"(&)" dog} {"(@)" emcat} {"(TU)" turtle} {"(SN)" snail} {"(BAH)" sheep} {":-\[" vampire} {":\[" vampire}
+	 {"(&)" dog} {"(@)" emcat} {"(tu)" turtle} {"(sn)" snail} {"(bah)" sheep} {":-\[" vampire} {":\[" vampire}
 	 
-	 {"(M)" messenger} {"(\{)" boyhug} {"(Z)" emboy} {"(\})" girlhug} {"(X)" emgirl} {"(Y)" thumbu} {"(N)" thumbd} 
-	 {"(YN)" fingerscrossed} {"(h5)" hifive}
+	 {"(m)" messenger} {"(\{)" boyhug} {"(Z)" emboy} {"(\})" girlhug} {"(x)" emgirl} {"(y)" thumbu} {"(n)" thumbd} 
+	 {"(yn)" fingerscrossed} {"(h5)" hifive}
 		   	  
 }
 
