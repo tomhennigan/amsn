eab974a029d07f54b032e96a79839c6750e86f41
airadier <airadier@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2003-08-05 15:24:32 +0000

    Improved tooltips look. Added an after cancel, or we had strange behavior.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@1072 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/balloon.tcl
+++ amsn/balloon.tcl
@@ -67,27 +67,35 @@ proc kill_balloon {} {
 
 proc balloon {target message {cx 0} {cy 0} } {
     global Bulle
+    after cancel "kill_balloon"
     if {$Bulle(first) == 1 } {
         set Bulle(first) 2
 	if { $cx == 0 && $cy == 0 } {
 	    set x [expr [winfo rootx $target] + ([winfo width $target]/2)]
-	    set y [expr [winfo rooty $target] + [winfo height $target] + 4]
+	    set y [expr [winfo rooty $target] + [winfo height $target] + 2]
 	} else {
-	    set x [expr $cx + 4]
-	    set y [expr $cy + 4]
+	    set x [expr $cx + 12]
+	    set y [expr $cy + 2]
 	}
 	if { [catch { toplevel .balloon -bg black}] != 0 } {
 	    destroy .balloon
 	    toplevel .balloon -bg black
 	}
-       
+
         wm overrideredirect .balloon 1
+
+	status_log "Screen: [winfo screenwidth $target]\n"
+	set wlength [expr {[winfo screenwidth $target] - $x - 5}]
+	if { $wlength < 100 } {
+	   set wlength 100
+	}
+
         label .balloon.l \
             -text $message -relief flat \
-            -bg #ffffaa -fg black -padx 2 -pady 0 -anchor w
-        pack .balloon.l -side left -padx 1 -pady 1
+            -bg #ffffaa -fg black -padx 2 -pady 0 -anchor w -font splainf -justify left -wraplength $wlength
+	pack .balloon.l -side left -padx 1 -pady 1
         wm geometry .balloon +${x}+${y}
-        set Bulle(set) 1
+	set Bulle(set) 1
 	after 10000 "kill_balloon"
     }
 }
