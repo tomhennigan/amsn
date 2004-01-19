82c30a35521e8b5548ed7d5868cda119c274b1ba
djrafa <djrafa@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-01-19 19:05:44 +0000

    Added amsn-update script
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@1808 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/utils/update-amsn.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+##############################################################################
+#This is a simple script to update aMsn from CVS and link
+#the skins in amsn-extras directory right.
+#
+#Rafael Rodríguez. apt-drink@telefonica.net
+#
+#v0.1: 18/01/03
+##############################################################################
+
+AMSNPATH=~/msn
+EXTRASPATH=~/msn/amsn-extras
+
+cd ${AMSNPATH}
+echo "Updating amsn..."
+cvs -f -q update -d -P "."
+cd ${EXTRASPATH}
+echo "Updating amsn-extras..."
+cvs -f -q update -d -P "."
+
+cd ${EXTRASPATH}/skins
+for SKIN in *
+do
+	if [ $SKIN != "CVS" ]; then
+		if [ ! -e "${AMSNPATH}/skins/${SKIN}" ]; then	#Link already exists?
+			echo "Linking skin ${SKIN}..."
+			ln -s ${EXTRASPATH}/skins/${SKIN} ${AMSNPATH}/skins/${SKIN}
+		else
+			echo "Not linking skin ${SKIN}. Link already exists..."
+		fi
+	fi
+done
+		
