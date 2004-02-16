ccff464a322f9af9456f077a22d7df760115c68d
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-02-16 21:37:46 +0000

    Fix sound about Mac OS X problem (yet AGAIN!)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@1978 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/gui.tcl
+++ amsn/gui.tcl
@@ -3713,9 +3713,11 @@ proc play_sound {sound_name} {
     if { $config(sound) == 1 } {
 	set sound [GetSkinFile sounds $sound_name]
 	catch {eval exec $config(soundcommand) &} res
- 		#Kill soundplayer after 30 second on Mac OS X (sometimes he stays open and eat your CPU)
+ 		#Kill soundplayer on Mac OS X (sometimes he stays open and eat your CPU)
 	if { $tcl_platform(os) == "Darwin" } {
 		after 30000 catch {exec killall -c sndplay}
+
+	}
     }
 }
 #///////////////////////////////////////////////////////////////////////
