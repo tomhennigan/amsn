df05bc77bacfc123baaa640c8dfe6c1d29ab41b9
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-03-16 18:22:11 +0000

    Fix sound problem on Mac OS X
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@2242 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/config.tcl
+++ amsn/config.tcl
@@ -449,8 +449,10 @@ proc load_config {} {
 				if { $bgcolormac=="#D8D8E0" } {
 					::config::setKey backgroundcolor #ECECEC
 					}
-			set soundmac [string range "[::config::getKey soundcommand]" 2 12]
-				if { $soundmac=="program_dir" } {
+			set soundmac [::config::getKey soundcommand]
+			set soundmac2 [string range "$soundmac" 1 11]
+			puts $soundmac2
+				if { $soundmac2=="program_dir" } {
 					::config::setKey soundcommand "./sndplay \$sound"
 					}
 		}
