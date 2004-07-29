e712f8eb02e5e198dd60cc4a8ad092cf42b93857
lio_lion <lio_lion@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-07-29 02:50:46 +0000

    corrected tabbing
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@2848 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/hotmail.tcl
+++ amsn/hotmail.tcl
@@ -1,215 +1,210 @@
 namespace eval ::hotmail {
-    if { $initialize_amsn == 1 } {
-	variable unread 0
-    }
+	if { $initialize_amsn == 1 } {
+		variable unread 0
+	}
 
-   proc unreadMessages {} {
-      variable unread
-      return $unread
-   }
+	proc unreadMessages {} {
+		variable unread
+		return $unread
+	}
 
 
-   proc setUnreadMessages { number } {
-      variable unread
-      set unread $number
-   }
+	proc setUnreadMessages { number } {
+		variable unread
+		set unread $number
+	}
 
-   proc composeMail { toaddr userlogin {pass ""} } {
-   # Note: pass can be empty, so user must enter password in the login
-   # page.
-   #
-   # $Id$
-   #
-      global tcl_platform HOME config
+	proc composeMail { toaddr userlogin {pass ""} } {
+		# Note: pass can be empty, so user must enter password in the login
+		# page.
+		#
+		# $Id$
+		#
+		global tcl_platform HOME config
 
-      if {$pass != ""} {
+		if {$pass != ""} {
 
-         set read_id [open "hotmlog.htm" r]
+			set read_id [open "hotmlog.htm" r]
 
-         set page_data ""
-         while {[gets $read_id tmp_data] != "-1"} {
-            set page_data "$page_data\n$tmp_data"
-         }
+			set page_data ""
+			while {[gets $read_id tmp_data] != "-1"} {
+				set page_data "$page_data\n$tmp_data"
+			}
 
-         close $read_id
+			close $read_id
 
-         #Here we calculate the creds and fields in the web page
-         set d(valid) Y
-         ::abook::getDemographics d
+			#Here we calculate the creds and fields in the web page
+			set d(valid) Y
+			::abook::getDemographics d
 
-         set userdata [split $userlogin "@"]
-         set email $userlogin
+			set userdata [split $userlogin "@"]
+			set email $userlogin
 
-         set login [lindex $userdata 0]
+			set login [lindex $userdata 0]
 
-         set kv $d(kv)
-         set sl [expr {[clock seconds] - $d(sessionstart)}]
-         set sid $d(sid)
-         set auth $d(mspauth)
-         set tomd5 $auth$sl$pass
-         set creds [::md5::md5 $tomd5]
+			set kv $d(kv)
+			set sl [expr {[clock seconds] - $d(sessionstart)}]
+			set sid $d(sid)
+			set auth $d(mspauth)
+			set tomd5 $auth$sl$pass
+			set creds [::md5::md5 $tomd5]
 
-         set url "/cgi-bin/compose?mailto=1&to=$toaddr"
+			set url "/cgi-bin/compose?mailto=1&to=$toaddr"
 
-         #Now let's substitute the $vars in hotmlog.htm
+			#Now let's substitute the $vars in hotmlog.htm
 
-         set page_data [subst -nocommands -nobackslashes $page_data]
+			set page_data [subst -nocommands -nobackslashes $page_data]
 
-         if {$tcl_platform(platform) == "unix"} {
-            set file_id [open "[file join ${HOME} hotlog.htm]" w 00600]
-         } else {
-            set file_id [open "[file join ${HOME} hotlog.htm]" w]
-         }
+			if {$tcl_platform(platform) == "unix"} {
+				set file_id [open "[file join ${HOME} hotlog.htm]" w 00600]
+			} else {
+				set file_id [open "[file join ${HOME} hotlog.htm]" w]
+			}
 
-         puts $file_id $page_data
+			puts $file_id $page_data
 
-         close $file_id
+			close $file_id
 
-     if {$tcl_platform(os) != "Darwin"} {
-     launch_browser "file://${HOME}/hotlog.htm" 1
-     } else {
-     launch_browser "${HOME}/hotlog.htm" 1
-     }
+			if {$tcl_platform(os) != "Darwin"} {
+				launch_browser "file://${HOME}/hotlog.htm" 1
+			} else {
+				launch_browser "${HOME}/hotlog.htm" 1
+			}
 
-      } else {
+		} else {
+			launch_browser "http://www.hotmail.com"
+		}
+	}
 
-	 launch_browser "http://www.hotmail.com"
-
-      }
-
-   }
-
-   proc viewProfile {user_login} {
-      launch_browser "http://members.msn.com/default.msnw?mem=${user_login}&pgmarket="
-   }
+	proc viewProfile {user_login} {
+		launch_browser "http://members.msn.com/default.msnw?mem=${user_login}&pgmarket="
+	}
 }
 
 proc hotmail_login {userlogin {pass ""}} {
-# Note: pass can be empty, so user must enter password in the login
-# page.
-#
-# $Id$
-#
-  global tcl_platform HOME config d
+	# Note: pass can be empty, so user must enter password in the login
+	# page.
+	#
+	# $Id$
+	#
+	global tcl_platform HOME config d
 
-  if {$pass != ""} {
+	if {$pass != ""} {
 
-    set read_id [open "hotmlog.htm" r]
+		set read_id [open "hotmlog.htm" r]
 
-    set page_data ""
-    while {[gets $read_id tmp_data] != "-1"} {
-      set page_data "$page_data\n$tmp_data"
-    }
+		set page_data ""
+		while {[gets $read_id tmp_data] != "-1"} {
+			set page_data "$page_data\n$tmp_data"
+		}
 
-    close $read_id
-
-
-    #Here we calculate the creds and fields in the web page
-    set d(valid) Y
-    ::abook::getDemographics d
-
-    set userdata [split $userlogin "@"]
-    set email $userlogin
-
-    set login [lindex $userdata 0]
-
-    set kv $d(kv)
-    set sl [expr {[clock seconds] - $d(sessionstart)}]
-    set sid $d(sid)
-    set auth $d(mspauth)
-    set tomd5 $auth$sl$pass
-    set creds [::md5::md5 $tomd5]
-
-    set url "/cgi-bin/HoTMaiL"
+		close $read_id
 
 
-    #Now let's substitute the $vars in hotmlog.htm
+		#Here we calculate the creds and fields in the web page
+		set d(valid) Y
+		::abook::getDemographics d
 
-    set page_data [subst -nocommands -nobackslashes $page_data]
+		set userdata [split $userlogin "@"]
+		set email $userlogin
 
-    if {$tcl_platform(platform) == "unix"} {
-      set file_id [open "[file join ${HOME} hotlog.htm]" w 00600]
-    } else {
-      set file_id [open "[file join ${HOME} hotlog.htm]" w]
-    }
+		set login [lindex $userdata 0]
 
-     puts $file_id $page_data
+		set kv $d(kv)
+		set sl [expr {[clock seconds] - $d(sessionstart)}]
+		set sid $d(sid)
+		set auth $d(mspauth)
+		set tomd5 $auth$sl$pass
+		set creds [::md5::md5 $tomd5]
 
-     close $file_id
-if {$tcl_platform(os) != "Darwin"} {
-     launch_browser "file://${HOME}/hotlog.htm" 1
-     } else {
-     launch_browser "${HOME}/hotlog.htm" 1
-     }
-     
-   } else {
-     launch_browser "http://www.hotmail.com"
-   }
+		set url "/cgi-bin/HoTMaiL"
 
+
+		#Now let's substitute the $vars in hotmlog.htm
+
+		set page_data [subst -nocommands -nobackslashes $page_data]
+
+		if {$tcl_platform(platform) == "unix"} {
+			set file_id [open "[file join ${HOME} hotlog.htm]" w 00600]
+		} else {
+			set file_id [open "[file join ${HOME} hotlog.htm]" w]
+		}
+
+		puts $file_id $page_data
+
+		close $file_id
+		if {$tcl_platform(os) != "Darwin"} {
+			launch_browser "file://${HOME}/hotlog.htm" 1
+		} else {
+			launch_browser "${HOME}/hotlog.htm" 1
+		}
+
+	} else {
+		launch_browser "http://www.hotmail.com"
+	}
 }
 
 
 proc hotmail_viewmsg {msgurl userlogin {pass ""}} {
-# Note: pass can be empty, so user must enter password in the login
-# page.
-#
-# $Id$
-#
-  global tcl_platform HOME config
+	# Note: pass can be empty, so user must enter password in the login
+	# page.
+	#
+	# $Id$
+	#
+	global tcl_platform HOME config
 
-  if {$pass != ""} {
+	if {$pass != ""} {
 
-    set read_id [open "hotmlog.htm" r]
+		set read_id [open "hotmlog.htm" r]
 
-    set page_data ""
-    while {[gets $read_id tmp_data] != "-1"} {
-      set page_data "$page_data\n$tmp_data"
-    }
+		set page_data ""
+		while {[gets $read_id tmp_data] != "-1"} {
+			set page_data "$page_data\n$tmp_data"
+		}
 
-    close $read_id
+		close $read_id
 
-    #Here we calculate the creds and fields in the web page
-    set d(valid) Y
-    ::abook::getDemographics d
+		#Here we calculate the creds and fields in the web page
+		set d(valid) Y
+		::abook::getDemographics d
 
-    set userdata [split $userlogin "@"]
-    set email $userlogin
+		set userdata [split $userlogin "@"]
+		set email $userlogin
 
-    set login [lindex $userdata 0]
+		set login [lindex $userdata 0]
 
-    set kv $d(kv)
-    set sl [expr {[clock seconds] - $d(sessionstart)}]
-    set sid $d(sid)
-    set auth $d(mspauth)
-    set tomd5 $auth$sl$pass
-    set creds [::md5::md5 $tomd5]
+		set kv $d(kv)
+		set sl [expr {[clock seconds] - $d(sessionstart)}]
+		set sid $d(sid)
+		set auth $d(mspauth)
+		set tomd5 $auth$sl$pass
+		set creds [::md5::md5 $tomd5]
 
-    set url $msgurl
+		set url $msgurl
 
-    #Now let's substitute the $vars in hotmlog.htm
+		#Now let's substitute the $vars in hotmlog.htm
 
-    set page_data [subst -nocommands -nobackslashes $page_data]
+		set page_data [subst -nocommands -nobackslashes $page_data]
 
-    if {$tcl_platform(platform) == "unix"} {
-      set file_id [open "[file join ${HOME} hotlog.htm]" w 00600]
-    } else {
-      set file_id [open "[file join ${HOME} hotlog.htm]" w]
-    }
+		if {$tcl_platform(platform) == "unix"} {
+			set file_id [open "[file join ${HOME} hotlog.htm]" w 00600]
+		} else {
+			set file_id [open "[file join ${HOME} hotlog.htm]" w]
+		}
 
-     puts $file_id $page_data
+		puts $file_id $page_data
 
-     close $file_id
+		close $file_id
 
-if {$tcl_platform(os) != "Darwin"} {
-     launch_browser "file://${HOME}/hotlog.htm" 1
-     } else {
-     launch_browser "${HOME}/hotlog.htm" 1
-     }
+		if {$tcl_platform(os) != "Darwin"} {
+			launch_browser "file://${HOME}/hotlog.htm" 1
+		} else {
+			launch_browser "${HOME}/hotlog.htm" 1
+		}
 
-   } else {
-     launch_browser "http://www.hotmail.com"
-   }
-
+	} else {
+		launch_browser "http://www.hotmail.com"
+	}
 }
 
 
@@ -271,45 +266,43 @@ proc hotmail_procmsg {msg} {
 	set content [::MSN::GetHeaderValue $msg Content-Type]
 
 	if {[string range $content 0 29] == "text/x-msmsgsemailnotification"} {     
-	  if {[::MSN::GetHeaderValue $msg From] != ""} {				
-	    set from [::MSN::GetHeaderValue $msg From]
-	    set fromaddr [::MSN::GetHeaderValue $msg From-Addr]
-	    if {[catch {set from [decode_from_field $from]} res]} {
-	    	status_log "Fail to decode from field: $res\n" res
-		set from $fromaddr
-	    }
-	    set msgurl [::MSN::GetHeaderValue $msg Message-URL]
-	    status_log "Hotmail: New mail from $from - $fromaddr\n"
+		if {[::MSN::GetHeaderValue $msg From] != ""} {				
+			set from [::MSN::GetHeaderValue $msg From]
+			set fromaddr [::MSN::GetHeaderValue $msg From-Addr]
+			if {[catch {set from [decode_from_field $from]} res]} {
+				status_log "Fail to decode from field: $res\n" res
+				set from $fromaddr
+			}
+			set msgurl [::MSN::GetHeaderValue $msg Message-URL]
+			status_log "Hotmail: New mail from $from - $fromaddr\n"
 
-            ::hotmail::setUnreadMessages [expr { [::hotmail::unreadMessages] + 1}]
+			::hotmail::setUnreadMessages [expr { [::hotmail::unreadMessages] + 1}]
 	
-	    if { $config(notifyemail) == 1 } {
-	    	::amsn::notifyAdd "[trans newmailfrom $from $fromaddr]" \
-	      	"hotmail_viewmsg $msgurl $config(login) $password" newemail
-	    }
+			if { $config(notifyemail) == 1 } {
+				::amsn::notifyAdd "[trans newmailfrom $from $fromaddr]" \
+				"hotmail_viewmsg $msgurl $config(login) $password" newemail
+			}
 
-	    cmsn_draw_online
+			cmsn_draw_online
 
-	  } 
+		} 
 	}
 	#Get the number of unread message in Inbox
 	if {[string range $content 0 36]  == "text/x-msmsgsinitialemailnotification"} {
-	  #Number of unread messages in inbox
-  	  set noleidos [::MSN::GetHeaderValue $msg Inbox-Unread]
-  	  #Get the URL of inbox directory in hotmail
-  	  set msgurl [::MSN::GetHeaderValue $msg Inbox-URL]
-	  status_log "Hotmail: $noleidos unread emails\n"
-	  #Remember the number of unread mails in inbox and create a notify window if necessary
-	  if { [string length $noleidos] > 0 } {
-	    ::hotmail::setUnreadMessages $noleidos
-	    cmsn_draw_online
-	    	if { $config(notifyemail) == 1} {
-	    		::amsn::notifyAdd "[trans newmail $noleidos]" \
-	      		"hotmail_login $config(login) $password" newemail
-		    }
-	  }
-	  
-	  
+		#Number of unread messages in inbox
+		set noleidos [::MSN::GetHeaderValue $msg Inbox-Unread]
+		#Get the URL of inbox directory in hotmail
+		set msgurl [::MSN::GetHeaderValue $msg Inbox-URL]
+		status_log "Hotmail: $noleidos unread emails\n"
+		#Remember the number of unread mails in inbox and create a notify window if necessary
+		if { [string length $noleidos] > 0 } {
+			::hotmail::setUnreadMessages $noleidos
+			cmsn_draw_online
+			if { $config(notifyemail) == 1} {
+				::amsn::notifyAdd "[trans newmail $noleidos]" \
+				"hotmail_login $config(login) $password" newemail
+			}
+		}
 	}
 	
 	#Get the number of unread message in others folders of Hotmail
@@ -321,29 +314,29 @@ proc hotmail_procmsg {msg} {
 		status_log "Hotmail: $folderunread unread emails in others folders \n"
 		#If the pref notifyemail is active and more than 0 email unread, show a notify on connect
 		if { $config(notifyemail) == 1 && [string length $folderunread] > 0} {
-	    	::amsn::notifyAdd "[trans newmailfolder $folderunread]" \
-	      	"hotmail_viewmsg $msgurl $config(login) $password" newemail
-	    }
+			::amsn::notifyAdd "[trans newmailfolder $folderunread]" \
+			"hotmail_viewmsg $msgurl $config(login) $password" newemail
+		}
 	}
 	
 	if {[string range $content 0 34]  == "text/x-msmsgsactivemailnotification"} {
-	  set source [::MSN::GetHeaderValue $msg Src-Folder]
-	  set dest [::MSN::GetHeaderValue $msg Dest-Folder]
-	  set delta [::MSN::GetHeaderValue $msg Message-Delta]
-	  if { $source == "ACTIVE" } {
-  	    set unread [expr {[::hotmail::unreadMessages] - $delta}]
-	  } elseif {$dest == "ACTIVE"} {
-  	    set unread [expr {[::hotmail::unreadMessages] + $delta}]
-	  } else {
-	    set unread [::hotmail::unreadMessages]
-	  }
-	  status_log "Hotmail num of messages changed: $unread unread emails\n"
-	  if { [string length $unread] > 0 } {
-	    ::hotmail::setUnreadMessages $unread
-	    cmsn_draw_online
-	  }
+		set source [::MSN::GetHeaderValue $msg Src-Folder]
+		set dest [::MSN::GetHeaderValue $msg Dest-Folder]
+		set delta [::MSN::GetHeaderValue $msg Message-Delta]
+		if { $source == "ACTIVE" } {
+			set unread [expr {[::hotmail::unreadMessages] - $delta}]
+		} elseif {$dest == "ACTIVE"} {
+			set unread [expr {[::hotmail::unreadMessages] + $delta}]
+		} else {
+			set unread [::hotmail::unreadMessages]
+		}
+		status_log "Hotmail num of messages changed: $unread unread emails\n"
+		if { [string length $unread] > 0 } {
+			::hotmail::setUnreadMessages $unread
+			cmsn_draw_online
+		}
 	}
-		#End by AIM
+	#End by AIM
 	# dock mail icon 
 	send_dock "MAIL" [::hotmail::unreadMessages]
 	status_log "hotmail_procmsg: Finishing\n"
