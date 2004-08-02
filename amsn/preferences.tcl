138dcb3bc9f16f20d46aae1ab7b6fc29ed203c6a
lio_lion <lio_lion@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-08-02 10:48:47 +0000

    Removed -textvariable config(...) replaced with -variable [::config::getVar ...]
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@2875 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/preferences.tcl
+++ amsn/preferences.tcl
@@ -473,12 +473,12 @@ proc Preferences { { settings "personal"} } {
 	frame $lfname.2 -class Degt
 	frame $lfname.3 -class Degt
 	checkbutton $lfname.1.lautonoact -text "[trans autonoact]" -onvalue 1 -offvalue 0 -variable [::config::getVar autoidle] -command UpdatePreferences
-	entry $lfname.1.eautonoact -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable config(idletime)
+	entry $lfname.1.eautonoact -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable [::config::getVar idletime]
 	label $lfname.1.lmins -text "[trans mins]" -padx 5
 	pack $lfname.1 -side top -padx 0 -expand 1 -fill both
 	pack $lfname.1.lautonoact $lfname.1.eautonoact $lfname.1.lmins -side left
 	checkbutton $lfname.2.lautoaway -text "[trans autoaway]" -onvalue 1 -offvalue 0 -variable [::config::getVar autoaway] -command UpdatePreferences
-	entry $lfname.2.eautoaway -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable config(awaytime)
+	entry $lfname.2.eautoaway -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable [::config::getVar awaytime]
 	label $lfname.2.lmins -text "[trans mins]" -padx 5
 	pack $lfname.2 -side top -padx 0 -expand 1 -fill both
 	pack $lfname.2.lautoaway $lfname.2.eautoaway $lfname.2.lmins -side left
@@ -802,14 +802,14 @@ proc Preferences { { settings "personal"} } {
         checkbutton $lfname.1.autoaccept -text "[trans autoacceptft]" -onvalue 1 -offvalue 0 -variable [::config::getVar autoaccept]
 	frame $lfname.1.ftport -class Deft
 	label $lfname.1.ftport.text -text "[trans ftportpref] :" -padx 5 -font splainf
-	entry $lfname.1.ftport.entry -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0 -width 5 -textvariable config(initialftport)
+	entry $lfname.1.ftport.entry -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0 -width 5 -textvariable [::config::getVar initialftport]
 	grid $lfname.1.ftport.text -row 1 -column 1 -sticky w -pady 5 -padx 0
 	grid $lfname.1.ftport.entry -row 1 -column 2 -sticky w -pady 5 -padx 3
 
         checkbutton $lfname.1.autoip -text "[trans autodetectip]" -onvalue 1 -offvalue 0 -variable [::config::getVar autoftip] -command UpdatePreferences
 	frame $lfname.1.ipaddr -class Deft
 	label $lfname.1.ipaddr.text -text "[trans ipaddress] :" -padx 5 -font splainf
-	entry $lfname.1.ipaddr.entry -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0 -width 15 -textvariable config(manualip)
+	entry $lfname.1.ipaddr.entry -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0 -width 15 -textvariable [::config::getVar manualip]
 	grid $lfname.1.ipaddr.text -row 1 -column 1 -sticky w -pady 5 -padx 0
 	grid $lfname.1.ipaddr.entry -row 1 -column 2 -sticky w -pady 5 -padx 3	
 		
@@ -871,13 +871,13 @@ proc Preferences { { settings "personal"} } {
 	frame $lfname.1 -class Degt
 	pack $lfname.1 -anchor w -side left -padx 0 -pady 5 -expand 0 -fill both
 	label $lfname.1.lbrowser -text "[trans browser] :" -padx 5 -font sboldf
-	entry $lfname.1.browser -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable config(browser)
+	entry $lfname.1.browser -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable [::config::getVar browser]
 	label $lfname.1.lbrowserex -text "[trans browserexample]" -font examplef
 	label $lfname.1.lfileman -text "[trans fileman] :" -padx 5 -font sboldf
-	entry $lfname.1.fileman -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable config(filemanager)
+	entry $lfname.1.fileman -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable [::config::getVar filemanager]
 	label $lfname.1.lfilemanex -text "[trans filemanexample]" -font examplef
 	label $lfname.1.lmailer -text "[trans mailer] :" -padx 5 -font sboldf
-	entry $lfname.1.mailer -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable config(mailcommand)
+	entry $lfname.1.mailer -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable [::config::getVar mailcommand]
 	label $lfname.1.lmailerex -text "[trans mailerexample]" -font examplef
 	
 	#aMSN for Mac OS X always use "QuickTimeTCL" (except in Alarms) so don't let mac user choose sound player
@@ -891,7 +891,7 @@ proc Preferences { { settings "personal"} } {
 		pack $lfname.1.sound.snack -anchor w -side top -padx 10
 		radiobutton $lfname.1.sound.other -text "[trans useother]" -value 0 -variable [::config::getVar usesnack] -command UpdatePreferences
 		pack $lfname.1.sound.other -anchor w -side top -padx 10
-		entry $lfname.1.sound.sound -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable config(soundcommand)
+		entry $lfname.1.sound.sound -bg #FFFFFF -bd 1 -highlightthickness 0 -width 40 -textvariable [::config::getVar soundcommand]
 		pack $lfname.1.sound.sound -anchor w -side top -padx 10
 		label $lfname.1.sound.lsoundex -text "[trans soundexample]" -font examplef
 		pack $lfname.1.sound.lsoundex -anchor w -side top -padx 10
@@ -931,7 +931,7 @@ proc Preferences { { settings "personal"} } {
 	button $lfname.1.browsetls -text [trans browse] -command "Browse_Dialog_dir libtls_temp"
 
 	label $lfname.1.lconvertpath -text "CONVERT" -padx 5 -font sboldf
-	entry $lfname.1.convertpath -bg #FFFFFF -bd 1 -width 45 -highlightthickness 0 -textvariable config(convertpath)
+	entry $lfname.1.convertpath -bg #FFFFFF -bd 1 -width 45 -highlightthickness 0 -textvariable [::config::getVar convertpath]
 	label $lfname.1.lconvertpathexp -text [trans convertexplain] -justify left -font examplef
 	button $lfname.1.browseconv -text [trans browse] -command "Browse_Dialog_file config(convertpath)"
 
@@ -971,9 +971,9 @@ proc Preferences { { settings "personal"} } {
 	frame $path.2 -class Degt
 
 	label $path.2.delimiters -text "[trans delimiters]" -padx 5
-	entry $path.2.ldelimiter -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable config(leftdelimiter)
+	entry $path.2.ldelimiter -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable [::config::getVar leftdelimiter]
 	label $path.2.example -text "HH:MM:SS" -padx 5
-	entry $path.2.rdelimiter -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable config(rightdelimiter)
+	entry $path.2.rdelimiter -bg #FFFFFF -bd 1 -font splainf -highlightthickness 0  -width 3 -textvariable [::config::getVar rightdelimiter]
 	pack $path.2 -side top -padx 0 -fill x
 	pack $path.2.delimiters $path.2.ldelimiter $path.2.example $path.2.rdelimiter -side left
 
@@ -1129,10 +1129,10 @@ proc Preferences { { settings "personal"} } {
         #label $lfname.check.linter3 -text "[trans blockinter3]"
         #label $lfname.check.linter4 -text "[trans blockinter4]"
         #label $lfname.check.lusers -text "[trans blockusers]"
-        #entry $lfname.check.inter1 -validate all -vcmd "BlockValidateEntry %W %P 1" -invcmd "BlockValidateEntry %W %P 0 15" -width 4 -textvariable config(blockinter1)
-        #entry $lfname.check.inter2 -validate all -vcmd "BlockValidateEntry %W %P 2" -invcmd "BlockValidateEntry %W %P 0 30"  -width 4 -textvariable config(blockinter2)
-        #entry $lfname.check.inter3 -validate all -vcmd "BlockValidateEntry %W %P 3" -invcmd "BlockValidateEntry %W %P 0 2" -width 4 -textvariable config(blockinter3)
-        #entry $lfname.check.users -validate all -vcmd "BlockValidateEntry %W %P 4" -invcmd "BlockValidateEntry %W %P 0 5" -width 4 -textvariable config(blockusers)
+        #entry $lfname.check.inter1 -validate all -vcmd "BlockValidateEntry %W %P 1" -invcmd "BlockValidateEntry %W %P 0 15" -width 4 -textvariable [::config::getVar blockinter1]
+        #entry $lfname.check.inter2 -validate all -vcmd "BlockValidateEntry %W %P 2" -invcmd "BlockValidateEntry %W %P 0 30"  -width 4 -textvariable [::config::getVar blockinter2]
+        #entry $lfname.check.inter3 -validate all -vcmd "BlockValidateEntry %W %P 3" -invcmd "BlockValidateEntry %W %P 0 2" -width 4 -textvariable [::config::getVar blockinter3]
+        #entry $lfname.check.users -validate all -vcmd "BlockValidateEntry %W %P 4" -invcmd "BlockValidateEntry %W %P 0 5" -width 4 -textvariable [::config::getVar blockusers]
 
         #grid $lfname.check.linter1 -row 1 -column 1 -sticky w
         #grid $lfname.check.linter2 -row 1 -column 3 -sticky w
@@ -1457,7 +1457,7 @@ proc UpdatePreferences {} {
 		$lfname.1.ipaddr.entry insert end [::config::getKey myip]
 		$lfname.1.ipaddr.entry configure -state disabled
 	} else {
-		$lfname.1.ipaddr.entry configure -state normal -textvariable config(manualip)
+		$lfname.1.ipaddr.entry configure -state normal -textvariable [::config::getVar manualip]
 	}
 
 	# remote control
