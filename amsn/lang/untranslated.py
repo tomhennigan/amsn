91f29a01610edb0b26d0a4f1a87c6a4499b6c332
hatredman <hatredman@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-08-05 20:37:41 +0000

    Sending files Qui Ago  5 17:37:32 BRT 2004. - By Hatredman
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@2919 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/lang/untranslated.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# Author: Gustavo Carneiro
+# Modified by: Alvaro J. Iradier
+# Use: missing.py langXX
+# lists all missing translations keys in language
+
+import string
+import fileinput
+
+msg_list = {}
+
+
+for line in fileinput.input("langen"):
+    i = string.find(line, ' ')
+    if i < 0: continue
+    key = line[:i]
+    val = line[i+1:]
+    msg_list[key] = val
+
+
+
+for line in fileinput.input():
+    tokens = string.split(line)
+    key = tokens[0]
+    try:
+	del msg_list[key]
+    except KeyError: pass
+    # print string.rstrip(line)
+
+print "Keys needing translation:"
+cont=0
+for key, val in msg_list.items():
+    cont=cont+1
+    print string.rstrip(key)
+    
+print
+print "---------------------------------------------------"
+print "There are still",cont,"missing (untranslated) keys."
