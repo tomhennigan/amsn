f3a92d6118840e12204496f40774540bc627a2c5
lio_lion <lio_lion@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-10-21 02:55:47 +0000

    add onclick
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3206 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/pop3/pop3.tcl
+++ amsn-extras/plugins/pop3/pop3.tcl
@@ -4,41 +4,45 @@
 #          POP3 Code from Tclib project               #
 #######################################################
 namespace eval ::pop3 {
-    variable config
-    variable configlist
-    variable emails 0
+	variable config
+	variable configlist
+	variable emails 0
 
-        #######################################################################################
-        #######################################################################################
-        ####            Initialization Procedure (Called by the Plugins System)            ####
-        #######################################################################################
-        #######################################################################################
+	#######################################################################################
+	#######################################################################################
+	####            Initialization Procedure (Called by the Plugins System)            ####
+	#######################################################################################
+	#######################################################################################
 
 	proc Init { dir } {
-	        ::plugins::RegisterPlugin pop3
-        	::plugins::RegisterEvent pop3 OnConnect start
-	        ::plugins::RegisterEvent pop3 Load start
-        	::plugins::RegisterEvent pop3 OnDisconnect stop
-	        ::plugins::RegisterEvent pop3 ContactListColourBarDrawn draw
-        	::plugins::RegisterEvent pop3 ContactListEmailsDraw addhotmail
+		::plugins::RegisterPlugin pop3
+		::plugins::RegisterEvent pop3 OnConnect start
+		::plugins::RegisterEvent pop3 Load start
+		::plugins::RegisterEvent pop3 OnDisconnect stop
+		::plugins::RegisterEvent pop3 ContactListColourBarDrawn draw
+		::plugins::RegisterEvent pop3 ContactListEmailsDraw addhotmail
 
-	        array set ::pop3::config {
-        	        host {"your.mailserver.here"}
-                	user {"user_login@here"}
-	                pass {""}
-        	        port {110}
-                	minute {5}
-	                notify {1}
-        	}
+		array set ::pop3::config {
+			host {"your.mailserver.here"}
+			user {"user_login@here"}
+			pass {""}
+			port {110}
+			minute {5}
+			notify {1}
+			loadMailProg {0}
+			mailProg {msimn}
+		}
 
-	        set ::pop3::configlist [list \
-        	        [list str "Check for new messages every ? minutes" minute] \
-                	[list str "POP3 Server"  host] \
-	                [list str "Your user login" user] \
-        	        [list pass "Your password" pass] \
-                	[list str "Port (optional)" port] \
-	                [list bool "Show notify window" notify] \
-        	]
+		set ::pop3::configlist [list \
+			[list str "Check for new messages every ? minutes" minute] \
+			[list str "POP3 Server"  host] \
+			[list str "Your user login" user] \
+			[list pass "Your password" pass] \
+			[list str "Port (optional)" port] \
+			[list bool "Show notify window" notify] \
+			[list bool "Load mail program on click (windows only)" loadMailProg] \
+			[list str "          Mail Program" mailProg] \
+		]
 	}
 
 
@@ -283,25 +287,27 @@ namespace eval ::pop3 {
 	#	An integer variable wich contains the number of mails in the mbox
 	proc ::pop3::check { } {
 
-		set chan [::pop3::open $::pop3::config(host) $::pop3::config(user) $::pop3::config(pass) $::pop3::config(port)]
-		set mails [::pop3::status $chan]
-		::pop3::close $chan
+		catch {
+			set chan [::pop3::open $::pop3::config(host) $::pop3::config(user) $::pop3::config(pass) $::pop3::config(port)]
+			set mails [::pop3::status $chan]
+			::pop3::close $chan
 
-		if { $::pop3::emails != $mails } {
-			set ::pop3::emails $mails
-			cmsn_draw_online
+			if { $::pop3::emails != $mails } {
+				set ::pop3::emails $mails
+				cmsn_draw_online
 
-			if { $::pop3::config(notify) == 1 && $mails != 0 } {
-				::amsn::notifyAdd "POP3\n[trans newmail $mails]" "" "" plugins
+				if { $::pop3::config(notify) == 1 && $mails != 0 } {
+					::amsn::notifyAdd "POP3\n[trans newmail $mails]" "" "" plugins
+				}
+				status_log "POP3 messages: $mails\n"
 			}
-			status_log "POP3 messages: $mails\n"
+
+			# Call itself again after x minutes
+			set time [expr {int($::pop3::config(minute) *60000)}]
+			after $time ::pop3::check
+
+			return $mails
 		}
-
-		# Call itself again after x minutes
-		set time [expr {int($::pop3::config(minute) *60000)}]
-		catch { after $time ::pop3::check }
-
-		return $mails
 	}
 
 
@@ -328,6 +334,20 @@ namespace eval ::pop3 {
 	}
 
 
+	# ::pop3::loadDefaultEmail
+	# Description:
+	#	Loads the default email program for the system
+	# Arguments: none
+	proc loadDefaultEmail { } {
+		if { $::tcl_platform(platform) == "windows" } {
+			if { [catch { WinLoadFile $::pop3::config(mailProg) } ] } {
+				load [file join plugins winutils winutils.dll]
+				WinLoadFile "msimn"
+			}
+		}
+	}
+
+
 	# ::pop3::draw
 	# Description:
 	#	Adds a line in the contact list of the number of emails drawn
@@ -336,9 +356,6 @@ namespace eval ::pop3 {
 	#     evPar   -> The array of parameters (Supplied by Plugins System)
 	proc draw {event evPar} {		
 		upvar 2 $evPar vars
-
-		label $vars(text).popmailpic -image [::skin::loadPixmap mailbox] -background white
-		$vars(text) window create end -window $vars(text).popmailpic -padx 3 -pady 0 -align center -stretch true
 
 		if { $::pop3::emails == 0 } {
 			set mailmsg "[trans nonewmail] (POP3)"
@@ -349,10 +366,26 @@ namespace eval ::pop3 {
 		} else {
 			set mailmsg "[trans newmail $::pop3::emails] (POP3)"
 		}
-
 		set maxw [expr [winfo width $vars(text)] -30]
 		set short_mailmsg [trunc $mailmsg $vars(text) $maxw splainf]
-		$vars(text) insert end "$short_mailmsg\n"
+
+		if { $::pop3::config(loadMailProg) } {
+			clickableImage $vars(text) popmailpic mailbox "::pop3::loadDefaultEmail" 5 0
+
+			#Set up TAGS for mail notification
+			$vars(text) tag conf pop3mail -fore black -underline true -font splainf
+			$vars(text) tag bind pop3mail <Button1-ButtonRelease> "$vars(text) conf -cursor watch; ::pop3::loadDefaultEmail"
+			$vars(text) tag bind pop3mail <Enter> "$vars(text) tag conf pop3mail -under false;$vars(text) conf -cursor hand2"
+			$vars(text) tag bind pop3mail <Leave> "$vars(text) tag conf pop3mail -under true;$vars(text) conf -cursor left_ptr"
+
+			$vars(text) insert end "$short_mailmsg\n"  pop3mail
+			$vars(text) tag add dont_replace_smileys pop3mail.first pop3mail.last
+		} else {
+			label $vars(text).popmailpic -image [::skin::loadPixmap mailbox] -background white
+			$vars(text) window create end -window $vars(text).popmailpic -padx 3 -pady 0 -align center -stretch true
+
+			$vars(text) insert end "$short_mailmsg\n"
+		}
 
 		if { ![::config::getKey checkemail] } {
 			$vars(text) insert end "\n"
