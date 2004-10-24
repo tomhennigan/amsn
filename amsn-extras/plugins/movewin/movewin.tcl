5fefa2fe7a1b7cc71d188f0f89f948a544e274cd
lio_lion <lio_lion@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-10-24 14:06:45 +0000

    initial version
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3240 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn-extras/plugins/movewin/movewin.tcl
@@ -0,0 +1,56 @@
+namespace eval ::movewin {
+	variable config
+	variable configlist
+
+	proc Init { dir } {
+		::plugins::RegisterPlugin movewin
+		::plugins::RegisterEvent movewin new_chatwindow move
+		::plugins::RegisterEvent movewin chat_msg_received message
+
+		array set ::movewin::config {
+			x {0}
+			y {0}
+			state {}
+		}
+
+		set ::movewin::configlist [list \
+			[list str "x position" x] \
+			[list str "y position" y] \
+			[list str "only in state (blank for any)" state] \
+		]
+	}
+
+	proc ::movewin::on { } {
+		global automessage
+
+		if { $::movewin::config(state) == "" } {
+			return 1
+		}
+
+		if { [info exists automessage] && $automessage != -1 \
+			&& [lindex $automessage 0] == $::movewin::config(state) \
+		   } {
+			return 1
+		} else {
+			return 0
+		}		
+	}
+
+	proc ::movewin::move { event epvar } {
+		upvar 2 $epvar vars
+
+		if { [::movewin::on] } {
+			wm geometry $vars(win) +$::movewin::config(x)+$::movewin::config(y)
+			wm state $vars(win) normal
+		}
+	}
+
+	proc ::movewin::message { event epvar } {
+		upvar 2 $epvar vars
+		upvar 2 $vars(chatid) chatid
+
+		if { [::movewin::on] } {
+			wm state [::ChatWindow::For $chatid] normal
+		}
+	}
+}
