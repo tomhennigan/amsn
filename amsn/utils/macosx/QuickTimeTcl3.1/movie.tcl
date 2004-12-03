ff3a4576b949676faf64f1b7c497521fca089044
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-12-03 15:21:46 +0000

    Add quicktime to utils/macosx
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3429 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/utils/macosx/QuickTimeTcl3.1/movie.tcl
@@ -0,0 +1,71 @@
+# movie.tcl
+#
+# This file defines the default bindings for the movie widget and provides
+# procedures that help in implementing those bindings.
+#
+# Copyright (c) 2004 Mats Bengtsson
+#
+# See the file "license.terms" for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+#
+# $Id$
+
+#-------------------------------------------------------------------------
+# The code below creates the default class bindings for movies.
+#-------------------------------------------------------------------------
+
+
+bind Movie <Right> {
+    if {[%W ispanoramic]} {
+	%W pan [expr [%W pan] - 10]
+    } else {
+	%W step 1
+    }
+}
+bind Movie <Left> {
+    if {[%W ispanoramic]} {
+	%W pan [expr [%W pan] + 10]
+    } else {
+	%W step -1
+    }
+}
+bind Movie <Up> {
+    if {[%W ispanoramic]} {
+	%W tilt [expr [%W tilt] + 10]
+    } else {
+	set vol [expr 64 + [%W cget -volume]]
+	if {$vol > 255} {set vol 255}
+	%W configure -volume $vol
+    }
+}
+bind Movie <Down> {
+    if {[%W ispanoramic]} {
+	%W tilt [expr [%W tilt] - 10]
+    } else {
+	set vol [expr -64 + [%W cget -volume]]
+	if {$vol < 0} {set vol 0}
+	%W configure -volume $vol
+    }
+}
+bind Movie <Shift_L> {
+    if {[%W ispanoramic]} {
+	%W fieldofview [expr [%W fieldofview] - 5]
+    }
+}
+bind Movie <Control_L> {
+    if {[%W ispanoramic]} {
+	%W fieldofview [expr [%W fieldofview] + 5]
+    }
+}
+bind Movie <space> {
+    if {![%W ispanoramic]} {
+	if {[%W rate] == 0} {
+	    %W play
+	} else {
+	    %W stop
+	}
+    }
+}
+
+
+
