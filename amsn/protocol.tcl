f83abbeffc2c3f4287e021a442033d3f1df7a9b7
gadget_boy <gadget_boy@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-12-10 16:42:49 +0000

    When moving a contact from one group to another, now the contact is added to the new group and then deleted from the former one
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3515 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/protocol.tcl
+++ amsn/protocol.tcl
@@ -1031,8 +1031,8 @@ namespace eval ::MSN {
 		if { $userName == "" } {
 			set userName $passport
 		}
-		set rtrid [::MSN::WriteSB ns "REM" "FL $passport $oldGid"]
 		set atrid [::MSN::WriteSB ns "ADD" "FL $passport [urlencode $userName] $newGid"]
+		set rtrid [::MSN::WriteSB ns "REM" "FL $passport $oldGid"]
 
 	}
 
