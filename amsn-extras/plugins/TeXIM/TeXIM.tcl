bc72ca5aa9acf12a8ff014e61a45615bc93da12f
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-12-11 04:21:08 +0000

    LaTex plugin
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3520 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn-extras/plugins/TeXIM/TeXIM.tcl
@@ -0,0 +1,103 @@
+# Copyright Andrei Barbu (abarbu@uwaterloo.ca)
+# This code is distributed under the GPL
+
+
+namespace eval ::TeXIM {
+
+	# We do this so more than one person can use amsn and this plugin concurrently
+	# Otherwise we would risk simultaneous access to the same file
+	variable localpid [pid]
+	variable dir /tmp/${localpid}-latex
+
+	proc Init { dir } {
+		plugins_log "TeXIM" "LaTeX plugin has started"
+
+		file mkdir $::TeXIM::dir
+
+		::plugins::RegisterPlugin "TeXIM"
+		::plugins::RegisterEvent "TeXIM" WinWrite parseLaTeX	
+
+    #TODO, fix amsn string support, it doesn't put it by default to what it sais in config
+    #TODO, add a multiline input box for header and footer
+
+                array set ::TeXIM::config {
+	            showtex {0}
+		    showerror {0}
+		    path_latex{"latex"}
+		    path_dvips{"dvips"}
+		    path_convert{"convert"}
+		    dummy{0}
+	        }
+													
+	        set ::TeXIM::configlist [list \
+    		[list bool "Show tex" showtex] \
+		[list bool "Display errors" showerror] \
+		[list str "Path to latex binary" path_latex] \
+		[list str "Path to dvips binary" path_dvips] \
+		[list str "Path to convert binary" path_convert] \
+	        ]
+	}
+	
+	proc DeInit { } {
+	    file delete -force $::TeXIM::dir
+	}
+	
+	proc parseLaTeX {event evPar} {
+
+	    upvar 2 txt txt
+	    upvar 2 win_name win_name
+
+	    if { [string first "\\tex " $txt] == 0 } { 
+		
+		# Strip \tex out
+		set msg [string range $txt 4 end]
+		
+		set localtime [clock seconds]
+
+		set chan [open ${::TeXIM::dir}/temp.tex w]
+		puts $chan "\\documentclass\[10pt\]\{article\}"
+		puts $chan "\\pagestyle{empty}"
+		puts $chan "\\begin\{document\}"
+		puts $chan "\\begin\{huge\}"
+		puts $chan $msg
+		puts $chan "\\end\{huge\}"
+		puts $chan "\\end\{document\}"
+		flush $chan
+		close $chan
+
+		if { [ catch { exec $::TeXIM::config(path_latex)  \
+			-output-directory=${::TeXIM::dir}/ -interaction=nonstopmode ${::TeXIM::dir}/temp.tex } msg ] == 0 } {
+		    if { [ catch { exec $::TeXIM::config(path_dvips) \
+			    -f -E -o ${::TeXIM::dir}/temp.ps -q ${::TeXIM::dir}/temp.dvi } msg ] == 0 } { 
+			if { [ catch { exec $::TeXIM::config(path_convert) \
+				 ${::TeXIM::dir}/temp.ps ${::TeXIM::dir}/temp.gif } msg ] == 0 } {
+				 
+		set imagename [image create photo -file ${::TeXIM::dir}/temp.gif -format gif]
+		
+                ${win_name}.f.out.text configure -state normal -font bplainf -foreground black
+		${win_name}.f.out.text image create end -name ${localtime} -image $imagename -padx 0 -pady 0 
+
+		if { $::TeXIM::config(showtex) == 0 } { 
+		    set txt "" 
+		} else {
+		    ${win_name}.f.out.text configure -state normal -font bplainf -foreground black
+		    ${win_name}.f.out.text insert end "\n"
+		}		    
+
+		return txt
+			}
+		    }
+		}
+		
+		if { $::TeXIM::config(showerror) == 1 } {
+		${win_name}.f.out.text insert end $msg
+		${win_name}.f.out.text insert end "\n"
+		} else { plugins_log "TeXIM" $msg } } 
+		
+		
+		return txt	
+	    }
+	    
+	    return ""
+	}
+}
