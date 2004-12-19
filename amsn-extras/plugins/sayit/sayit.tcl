6f8a2913f03c0cddb2b06110e4ec77fc466b3973
lio_lion <lio_lion@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2004-12-19 07:03:23 +0000

    Now also supports windows, and some code cleaning
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3574 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/sayit/sayit.tcl
+++ amsn-extras/plugins/sayit/sayit.tcl
@@ -1,51 +1,44 @@
 namespace eval ::sayit {
-    variable config
-    variable configlist
-    
+	variable config
+	variable configlist
 
 	proc InitPlugin { dir } {
-    	::plugins::RegisterPlugin sayit
-		source [file join $dir sayit.tcl]
-    	
-  
+		if { $::tcl_platform(platform) == "windows" } {
+			load [file join $dir winsayit.dll]
+		}
 
-    	::plugins::RegisterEvent sayit chat_msg_received newmessage
-  
- 
- 
-    	array set ::sayit::config {
-		voice {}
-		
-    	}
-    	
+		::plugins::RegisterPlugin sayit
+		::plugins::RegisterEvent sayit chat_msg_received newmessage
 
-    	set ::sayit::configlist [list \
-				  [list str "Voice"  voice] \
-				 ]
+		array set ::sayit::config {
+			voice {}
+		}
+
+		set ::sayit::configlist [list \
+			[list str "Voice (Mac only)"  voice] \
+		]
 	}
-	
 
-    proc newmessage {event evpar} {
-    	variable config
-    	upvar 2 evpar newvar
-    	upvar 2 msg msg
-    	upvar 2 user user
-    	#Define the 3 variables, email, nickname and message
-    	set email $user
-    	set nickname [::abook::getDisplayNick $user]
-    	
-    	
+	proc newmessage {event evpar} {
+		variable config
+		upvar 2 $evpar newvar
+		upvar 2 $newvar(message) msg
+		upvar 2 $newvar(user) user
 
-    		if { (($email != [::config::getKey login]) && [focus] == "") && $msg != "" } {
-    			if {$config(voice)!=""} {
-    			exec say -v $config(voice) $msg
-    			
-    			} else {
-    			exec say $msg
-    			
-    			}
-    		}
- 	}
+		#Define the 3 variables, email, nickname and message
+		set email $user
+		set nickname [::abook::getDisplayNick $user]
+
+		if { (($email != [::config::getKey login]) && [focus] == "") && $msg != "" } {
+			if { $::tcl_platform(platform) == "windows" } {
+				WinSayit $msg
+			} else {
+				if {$config(voice)!=""} {
+					exec say -v $config(voice) $msg
+				} else {
+					exec say $msg
+				}
+			}
+		}
 	}
-	
-        
+}
