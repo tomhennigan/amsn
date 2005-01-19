f9b213ee7aea6164c49d1ad8c5b7911397e5e5f9
roadkillbunny <roadkillbunny@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-01-19 04:43:44 +0000

    gename: bug found&fixed, added msn char limit check, added support for command-line switches
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@3829 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/gename/gename.tcl
+++ amsn-extras/plugins/gename/gename.tcl
@@ -1,9 +1,12 @@
 namespace eval ::gename {
     variable config
     variable configlist
+    variable try 0
 
     proc InitPlugin { dir } {
 	variable configlist
+	plugins_log gename "Welcome to Gename!\n"
+	::plugins::RegisterPlugin gename
 	::plugins::RegisterEvent gename ChangeState newname
 	array set ::gename::config {
 	    prefix {}
@@ -16,16 +19,37 @@ namespace eval ::gename {
     }
     
     proc newname {event epvar} {
+	set name [::gename::gename]
+	::MSN::changeName [::gename::get_login] $name
+	plugins_log gename "New name is set to $name"
+    }
+
+    proc gename { } {
 	variable config
-	if {[catch {exec $config(program)} ret] } {
-	    pluginss_log gename "Error executing $config(program)\n"
-	    return $ret
+	variable try
+	incr try
+	if {[catch "exec $config(program)" ret] } {
+	    plugins_log gename "Error executing $config(program) because $ret\n"
+	    return
 	}
 	set name "$config(prefix) "
 	append name $ret
+	
 	plugins_log gename "$config(program) returned $ret\n"
-	::MSN::changeName [::gename::get_login] $name
+
+	if {[string length "$name"] > 129 } {
+	    plugins_log gename "Damn! The new name is too long, regenerating...\n"
+	    if {$try > 5} {
+		plugins_log gename "Too many gename tries failed. Please check your configuration."
+	    } else {
+		set name [::gename::gename]
+	    }
+	}
+
+	set try 0
+	return $name
     }
+
     proc get_login {} {
 	global config
 	return $config(login)
