30ade20f9707b034878006b3a8da7531d4f3a576
bluetit <bluetit@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-02-09 13:09:41 +0000

    fixed different border width issues on windows
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@4086 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/balloon.tcl
+++ amsn/balloon.tcl
@@ -116,7 +116,12 @@ proc balloon {target message {cx 0} {cy 0} } {
         label .balloon.l \
 	    -text ${message} -relief flat \
 	    -bg [::skin::getKey balloonbackground] -fg [::skin::getKey balloontext] -padx 2 -pady 0 -anchor w -font [::skin::getKey balloonfont] -justify left -wraplength $wlength
-	set bw [expr [::skin::getKey balloonborderwidth] - 1]
+	if {![catch {tk windowingsystem} wsystem] && $wsystem == "windows" } {
+		set bw [expr [::skin::getKey balloonborderwidth]]
+	} else {
+		set bw [expr [::skin::getKey balloonborderwidth] - 1 ]
+	}
+	
 	pack .balloon.l -side left -padx $bw -pady $bw
         wm geometry .balloon +${x}+${y}
         
