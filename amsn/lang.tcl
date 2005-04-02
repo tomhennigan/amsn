f680a6d4ac9f8f1294d5a95ae3f228c9d6ef2268
gadget_boy <gadget_boy@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-04-02 09:25:51 +0000

    Fix a bug when sorting language
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@4320 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/lang.tcl
+++ amsn/lang.tcl
@@ -265,14 +265,21 @@ namespace eval ::lang {
 			set name [::lang::ReadOnlineLang $langcode name]
 			lappend languages2 [list "$name" "$langcode"]
 		}
-		set languages2 [lsort -index 0 -dictionary $languages2] 
+		set languages2 [lsort -index 0 -dictionary $languages2]
 		
+		set ::lang::OnlineLang [list]
+		
+		foreach lang $languages2 {
+			set langcode [lindex $lang 1]
+			set ::lang::OnlineLang [lappend ::lang::OnlineLang $langcode]
+		}		
 		
 		foreach item $languages2 {
 			set langname [lindex $item 0]
+			set langcode [lindex $item 1]
 			$frm.selection.box insert end "$langname"
 			# Choose the background according to the fact lang is available or not
-			if { [lsearch $::lang::Lang [lindex $item 0]] != -1 } {
+			if { [lsearch $::lang::Lang $langcode] != -1 } {
 				$frm.selection.box itemconfigure end -background #DDF3FE
 			} else {
 				$frm.selection.box itemconfigure end -background #FFFFFF
