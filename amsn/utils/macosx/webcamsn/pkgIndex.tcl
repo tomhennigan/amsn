dfc0922a8736541297e75b9e91a02552b232b944
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-04-12 02:33:24 +0000

    Commit webcamsn package compiled on Mac OS X (Using glib.h, not glib_replacement, Youness tell me when you are ready for that)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@4426 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/utils/macosx/webcamsn/pkgIndex.tcl
@@ -0,0 +1,6 @@
+# Tcl package index file, version 1.0
+
+if {[package vcompare [info tclversion] 8.4] < 0} return
+
+package ifneeded webcamsn 0.1 "package require Tk; [list load [file join $dir webcamsn.dylib] webcamsn]; 
+package provide webcamsn 0.1"
