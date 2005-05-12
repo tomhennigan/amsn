730df37685ca526b512fb1f50a4a62939a3e07ce
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-05-12 03:00:10 +0000

    Add a new proc: Resize with ratio
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@4658 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/picture.tcl
+++ amsn/picture.tcl
@@ -119,11 +119,71 @@ set ::tkcximageloaded 0
 				status_log "Picture.tcl: Unable to crop image with TkCxImage\n$res" red
 				return 0
 			} else {
-				return $temp
+				image create photo $photo
+				$photo copy $temp
+				return $photo
 			}
 		}
 	}
 	
+	#Resize a picture from photo to a specific size and keep the ratio
+	proc ResizeWithRatio {photo width height} {
+		if {[::picture::Loaded]} {
+			#Actual size of the photo
+			set origw [image width $photo]
+			set origh [image height $photo]
+			#status_log "picture.tcl: Image size is $origw $origh\n" red
+			
+			#Actual ratio of the photo
+			set origratio [expr { 1.0*$origw / $origh } ]
+			#New ratio
+			set ratio [expr { 1.0*$height / $width} ]
+			#status_log "picture.tcl: Original ratio is $origratio and new ratio: $ratio\n" red
+			
+			#Depending on ratio, resize to keep smaller dimension to XX pixels
+			if { $origratio > $ratio} {
+				set resizeh $height
+				set resizew [expr {round($resizeh*$origratio)}]
+			} else {
+				set resizew $width
+				set resizeh [expr {round($resizew/$origratio)}]
+			}
+			
+			#Resize the picture
+			if { $origw != $width || $origh != $height } {
+				#status_log "picture.tcl : Will resize to $resizew x $resizeh \n" red	
+				::picture::Resize $photo $resizew $resizeh
+			}
+			
+			#Now let's crop image from the center
+			set centerx [expr { [image width $photo] /2 } ]
+			set centery [expr { [image height $photo] /2 } ]
+			set halfw [expr { $width / 2}]
+			set halfh [expr { $height / 2}]
+			set x1 [expr {$centerx-$halfw}]
+			set y1 [expr {$centery-$halfh}]
+			if { $x1 < 0 } {
+				set x1 0
+			}
+			if { $y1 < 0 } {
+				set y1 0
+			}
+			
+			set x2 [expr {$x1+$width}]
+			set y2 [expr {$y1+$height}]
+			
+			set neww [image width $photo]
+			set newh [image height $photo]
+			
+			#status_log "picture.tcl: Resized image size is $neww $newh\n" red
+			#status_log "picture.tcl: Center of image is $centerx,$centery, will crop from $x1,$y1 to $x2,$y2 \n" red
+			
+			::picture::Crop $photo $x1 $y1 $x2 $y2
+			
+			return $photo
+			
+		}
+	}
 	#Save a picture to a file, from a photo
 	#Format supported: "cxgif" "cxpng" "cxjpg" "cxtga"
 	proc Save {photo destination {format ""}} {
