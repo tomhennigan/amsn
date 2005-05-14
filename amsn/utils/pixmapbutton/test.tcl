55f6c0801920b7571fe921552e12dee27e16ccb7
bluetit <bluetit@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-05-14 21:46:03 +0000

    changed demo
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@4679 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/pixmapbutton/test.tcl
+++ amsn/utils/pixmapbutton/test.tcl
@@ -6,22 +6,23 @@ package require pixmapbutton
 
 #wm attributes
 wm title . "button test"
-wm geometry . 400x45
+wm geometry . 150x45
 . config -bg white
 update
 
 font create plain -family helvetica -size 12 -weight normal
 font create massive -family helvetica -size 32
 image create photo icon -file icon.gif
+image create photo icon2 -file icon2.gif
 
-pixmapbutton .b1 -command "puts ok" -font plain
+pixmapbutton .b1 -command "puts ok" -font plain -emblemimage icon2 -emblempos "left center"
 
 pixmapbutton .b2 -command "puts cancel" -font plain -emblemimage icon -emblempos "left center" -fg red
 
-pack .b1 -side left -padx 5 -pady 5 -fill both -expand true
+pack .b1 -side left -padx 5 -pady 5 -fill none -expand false
 pack .b2 -side right -padx 5 -pady 5 -fill none -expand false
-.b1 configure -text "Fill both, disabled"
-.b2 configure -text "Fill none, with emblem"
+.b1 configure -text "Ok"
+.b2 configure -text "Cancel"
 #.ok configure -font massive
-.b1 configure -state disabled
-.b2 configure -fg white
+#.b1 configure -state disabled
+.b2 configure -fg black
