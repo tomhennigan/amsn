957671d130945c8dcf253d72e51b2dea7deaef29
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-05-30 13:51:56 +0000

    changed requirement of TkCximage to 8.3
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@4781 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/TkCximage/pkgIndex.tcl
+++ amsn/utils/TkCximage/pkgIndex.tcl
@@ -1,5 +1,5 @@
 # Tcl package index file, version 1.0
 
-if {[package vcompare [info tclversion] 8.4] < 0} return
+if {[package vcompare [info tclversion] 8.3] < 0} return
 
 package ifneeded TkCximage 0.2 "package require Tk; [list load [file join $dir TkCximage[info shared]] TkCximage]; package provide TkCximage 0.2"
