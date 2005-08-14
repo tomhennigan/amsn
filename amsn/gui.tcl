d0b0f1c468596e796a5301bcc9ea67208de6f996
germinator2000 <germinator2000@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-08-14 17:49:10 +0000

    New key: notify_dp_border For skins that use a border on notify_window Fix problem that Lz reported for people that do not want a picture
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@5123 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/gui.tcl
+++ amsn/gui.tcl
@@ -142,6 +142,7 @@ if { $initialize_amsn == 1 } {
 	::skin::setKey y_notifytext 22
 	::skin::setKey width_notifytext 93
 	::skin::setKey notify_font sboldf
+	::skin::setKey notify_dp_border 0
 
 	if {![catch {tk windowingsystem} wsystem] && $wsystem == "aqua"} {
 		::skin::setKey balloonbackground #ffffca
@@ -2931,14 +2932,28 @@ namespace eval ::amsn {
 			}
 		}
 		#If it's a notification with user variable and we get a sucessful image create, show the display picture in the notification
-		if {$user != "" && [getpicturefornotification $user] && [::config::getKey showpicnotify]} {
-			#Create image
-			$w.c create image [::skin::getKey x_notifydp] [::skin::getKey y_notifydp] -anchor nw -image smallpicture$user
+		if {$user != "" && [getpicturefornotification $user] || [::skin::getKey notify_dp_border]} {
+		
+			if {[::config::getKey showpicnotify]} {
+				#Create image
+				$w.c create image [::skin::getKey x_notifydp] [::skin::getKey y_notifydp] -anchor nw -image smallpicture$user
+				  			$w.c create image [::skin::getKey x_notifydp] [::skin::getKey y_notifydp] -anchor nw -image smallpicture$user
+				#Add text
+				set notify_id [$w.c create text [::skin::getKey x_notifytext] [::skin::getKey y_notifytext] \
+					-font [::skin::getKey notify_font] \
+					-justify left -width [::skin::getKey width_notifytext] -anchor nw -text "$msg"]
+			} else {
+				#Just add the text and use full width
+				set notify_id [$w.c create text [expr {[::skin::getKey notifwidth]/2}] 35 \
+					-font [::skin::getKey notify_font] \
+					-justify center -width [expr {[::skin::getKey notifwidth]-20}] -anchor n -text "$msg"]
+			}
+		} else {
+			#Just add the text and use full width
+			set notify_id [$w.c create text [expr {[::skin::getKey notifwidth]/2}] 35 \
+				-font [::skin::getKey notify_font] \
+				-justify center -width [expr {[::skin::getKey notifwidth]-20}] -anchor n -text "$msg"]
 		}
-
-		set notify_id [$w.c create text [::skin::getKey x_notifytext] [::skin::getKey y_notifytext] \
-			-font [::skin::getKey notify_font] \
-			-justify left -width [::skin::getKey width_notifytext] -anchor nw -text "$msg"]
 		
 		$w.c create image [::skin::getKey x_notifyclose] [::skin::getKey y_notifyclose] -anchor nw -image [::skin::loadPixmap notifclose]
 
