3662bee5c58f7e23ee2eaca30a8d3aaa134fef5f
roadkillbunny <roadkillbunny@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-12-29 21:26:58 +0000

    Added a margin on top and centered the footer
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@5696 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -11,10 +11,10 @@
 
 
     </div>
-    <div id="footer"><br />
+    <div id="footer" class="center"><br />
       All logos and trademarks in this site are property of their respective
       owner. Comments and news posts are the property of their posters, all
-      the rest 2002-2005 by the amsn team.
+      the rest 2002-2005 by the aMSN team.
     </div>
 </body>
 </html>
