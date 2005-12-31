ff1fde054ea534be1a137262ddff327c6227b853
roadkillbunny <roadkillbunny@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2005-12-31 08:19:30 +0000

    Linux downloads now work with a select box with JS and non-JS.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@5713 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/linux-downloads.php
+++ www/linux-downloads.php
@@ -22,50 +22,27 @@ function formHandler(form){
         window.location.href = URL;
 }
 </script>
-<noscript>
-<ul>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.linux-installer.bin">Linux Installer(need llibstdc++.so.6)</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95.x86.package">AutoPackage</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn_0.95-1.deb">Debian</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn_0.95-1.sarge.deb">Debian (Sarge)</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn_0.95-2.ubuntu.deb">Ubuntu</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn_0.95-ubuntu.powerpc.deb">Ubuntu (PowerPC)</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.fc3.i686.rpm">Fedora Core 3</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-2.fc4.i686.rpm">Fedora Core 4</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1-mdk92.i686.rpm">Mandrake 9.2</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1mdk.i686.rpm">Mandriva 2006 (Formerly Mandrake)</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-i486.tgz">Slackware</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.suse.i586.rpm">SUSE 10.0</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.archlinux.tar.gz">Archlinux</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95.ebuild">Gentoo</a></li>
-<li><a href="http://prdownloads.sourceforge.net/amsn/amsn-0.95.tar.gz">Other</a></li>
-</ul>
-</noscript>
-<form action="#">
+<form action="dlfile.php" method="GET">
 <p>
-<select id="Packages" onchange="javascript:formHandler()">
+<select name="file" onchange="this.form.submit()">
 <option value="" selected="selected" />
-
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.linux-installer.bin">Linux Installer (need llibstdc++.so.6)</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95.x86.package">AutoPackage</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn_0.95-1.deb">Debian</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn_0.95-1.sarge.deb">Debian (Sarge)</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn_0.95-2.ubuntu.deb">Ubuntu</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn_0.95-ubuntu.powerpc.deb">Ubuntu (PowerPC)</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.fc3.i686.rpm">Fedora Core 3</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-2.fc4.i686.rpm">Fedora Core 4</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1-mdk92.i686.rpm">Mandrake 9.2</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1mdk.i686.rpm">Mandriva 2006 (Formerly Mandrake)</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-i486.tgz">Slackware</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.suse.i586.rpm">SUSE 10.0</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95-1.archlinux.tar.gz">Archlinux</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95.ebuild">Gentoo</option>
-<option value="http://prdownloads.sourceforge.net/amsn/amsn-0.95.tar.gz">Other</option>
-
-
-
-<!--<option value="http://www.mteixeira.webset.net/amsn/">Conectiva-->
+<option value="amsn-0.95-1.linux-installer.bin">Bitrock Installer</option>
+<option value="amsn-0.95.x86.package">AutoPackage</option>
+<option value="amsn_0.95-1.deb">Debian</option>
+<option value="amsn_0.95-1.sarge.deb">Debian (Sarge)</option>
+<option value="amsn_0.95-2.ubuntu.deb">Ubuntu</option>
+<option value="amsn_0.95-ubuntu.powerpc.deb">Ubuntu (PowerPC)</option>
+<option value="amsn-0.95-1.fc3.i686.rpm">Fedora Core 3</option>
+<option value="amsn-0.95-2.fc4.i686.rpm">Fedora Core 4</option>
+<option value="amsn-0.95-1-mdk92.i686.rpm">Mandrake 9.2</option>
+<option value="amsn-0.95-1mdk.i686.rpm">Mandriva 2006 (Formerly Mandrake)</option>
+<option value="amsn-0.95-i486.tgz">Slackware</option>
+<option value="amsn-0.95-1.suse.i586.rpm">SUSE 10.0</option>
+<option value="amsn-0.95-1.archlinux.tar.gz">Archlinux</option>
+<option value="amsn-0.95.ebuild">Gentoo</option>
+<option value="amsn-0.95.tar.gz">Other</option>
 </select>
+<input type="submit" value="Download"/>
 </p>
 </form>
 
