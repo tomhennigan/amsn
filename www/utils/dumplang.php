c8ad6f67ec7d80f6cdbb6ad2b69788537c930441
roadkillbunny <roadkillbunny@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-01-07 19:03:21 +0000

    Dumping languages works now
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@5786 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/utils/dumplang.php
+++ www/utils/dumplang.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../common.php';
-
+echo 'TRUNCATE TABLE `amsn_langs`;';
 $query="SELECT * FROM amsn_langs";
 $result=mysql_query($query) or die(mysql_error());
 
@@ -9,11 +9,13 @@ while($row=mysql_fetch_array($result)) {
   $names='';
   $values='';
   foreach($row as $key=>$value) {
-    $names.=$key.",";
-    $values.="'".$value."',";
+    if($key!==intval($key) && $key!='lang_id') {
+      $names.=$key.",";
+      $values.="'".$value."',";
+    }
   }
   
 
-  echo substr($names,0,strlen($names)-1).') VALUES ('.substr($values,0,strlen($values)-1).")\n";
+  echo substr($names,0,strlen($names)-1).') VALUES ('.substr($values,0,strlen($values)-1).");\n";
 }
 ?>
