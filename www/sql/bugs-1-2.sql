b5c86caf09f83291b9777019c75ec2d5a4226cf1
roadkillbunny <roadkillbunny@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-02-05 06:40:04 +0000

    Forgot a ; in some places
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@6023 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/sql/bugs-1-2.sql
+++ www/sql/bugs-1-2.sql
@@ -10,9 +10,10 @@ CREATE TABLE `bugs` (
 ) TYPE = MYISAM ;
 ALTER TABLE `bugreports` DROP `bug_flag` ;
 ALTER TABLE `bugs` ADD `bug_name` VARCHAR( 50 ) NOT NULL DEFAULT 'New Bug' AFTER `bug_id` ;       
-ALTER TABLE `bugs` CHANGE `bug_id` `bug_id` INT( 20 ) NOT NULL AUTO_INCREMENT
+ALTER TABLE `bugs` CHANGE `bug_id` `bug_id` INT( 20 ) NOT NULL AUTO_INCREMENT;
 DROP TABLE `duplicates` ;
 ALTER TABLE `bugs` ADD `bug_desc` TEXT NOT NULL ;
-ALTER TABLE `bugs` CHANGE `bug_developer` `bug_developer` CHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'Nobody'
+ALTER TABLE `bugs` CHANGE `bug_developer` `bug_developer` CHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'Nobody';
 ALTER TABLE `bugs` ADD `bug_last_update` INT( 20 ) NOT NULL AFTER `bug_priority` ;
 DROP TABLE flags;
+ALTER TABLE `bugreports` ADD `bug_parent` INT( 20 ) NOT NULL ;
