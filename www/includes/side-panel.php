6f881dd8165d92b7ef7cd9388298a7a57cdfb5d4
roadkillbunny <roadkillbunny@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-02-05 20:50:09 +0000

    Some weird bug with include() in sf.net servers
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@6034 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/includes/side-panel.php
+++ www/includes/side-panel.php
@@ -4,8 +4,8 @@
 
 /* uncomment next line to enable language block in the sidebar */
 //include 'side-panel-language.php';
-include 'side-panel-polls.php';
-include 'side-panel-help.php';
-include 'side-panel-forums.php';
-include 'side-panel-links.php';
+include 'includes/side-panel-polls.php';
+include 'includes/side-panel-help.php';
+include 'includes/side-panel-forums.php';
+include 'includes/side-panel-links.php';
 ?>
