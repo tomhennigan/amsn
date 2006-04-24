3c202fdcc768b80863f3c262b00573ccfc1736c5
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-04-24 17:02:27 +0000

    Jonne s patch
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@6459 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/gename/gename.tcl
+++ amsn-extras/plugins/gename/gename.tcl
@@ -2,27 +2,57 @@ namespace eval ::gename {
     variable config
     variable configlist
     variable try 0
+	variable timer ""
 
     proc InitPlugin { dir } {
+
 	variable configlist
 	plugins_log gename "Welcome to Gename!\n"
 	::plugins::RegisterPlugin gename
 	::plugins::RegisterEvent gename ChangeState newname
+
 	array set ::gename::config {
 	    prefix {}
-	    program {/usr/bin/fortune}
+	    program {/usr/bin/fortune -s}
+		onStateChange 0
+		refreshRate 120
+		usePSM 1
 	}
 	set configlist [list \
 			    [list str "Prefix"  prefix] \
 			    [list str "Program" program] \
+				[list bool "Change on state change" onStateChange] \
+				[list str "Refresh rate (seconds, 0 means only once)" refreshRate] \
+				[list bool "use PSM (otherwise name is changed)" usePSM] \
 			   ]
+
+	after 5000 [list ::gename::newname "" "" 0]
     }
     
-    proc newname {event epvar} {
+    proc newname {event epvar {stateChange 1}} {
+	variable config
+	variable timer
 	set name [::gename::gename]
-	::MSN::changeName [::gename::get_login] $name
+	if {$stateChange == 1 && $config(onStateChange) == 0} {
+		return
+	}
+	if {$config(usePSM)} {
+		::MSN::changePSM $name
+	} else {
+		::MSN::changeName [::gename::get_login] $name
+	}
 	plugins_log gename "New name is set to $name"
+	if {$config(refreshRate) > 0} {
+		set timer \
+		[after [expr {1000*$config(refreshRate)}] [list ::gename::newname {} {} 0]]
+	}
     }
+
+	proc DeInitPlugin { } {
+		variable timer
+		# Cancel active timer
+		catch { after cancel $timer }
+	}
 
     proc gename { } {
 	variable config
