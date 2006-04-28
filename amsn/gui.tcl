7c62ef6e3557f490b427202c6dc2a2c8bde12e02
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-04-28 00:59:07 +0000

    missed sth :)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@6504 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/gui.tcl
+++ amsn/gui.tcl
@@ -2917,7 +2917,7 @@ namespace eval ::amsn {
 		}
 
 		if {[::config::getKey chatsmileys]} {
-			if {[::config::getKey customsmileys] && [::abook::getContactData $user showcustomsmileys] != 0 } {
+			if {([::config::getKey customsmileys] && [::abook::getContactData $user showcustomsmileys] != 0) || [::abook::getContactData $user showcustomsmileys] == 1 } {
 				custom_smile_subst $chatid [::ChatWindow::GetOutText ${win_name}] $text_start end
 			}
 			#Replace smileys... if you're sending custom ones, replace them too (last parameter)
