423a9250f928ab404a08c632559c7a8d8fe61854
lephilousophe <lephilousophe@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-06-15 17:04:31 +0000

    Made the download page Valid XHTML Strict
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@6882 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/download.php
+++ www/download.php
@@ -17,7 +17,7 @@
             <li><a href="dlfile.php?file=amsn-0.95-windows-installer-2.exe" class="screeny"><img class="thumb" src="images/download-windows.png" alt="Screenshot" /></a><br />Windows</li>
             <li><a href="dlfile.php?file=amsn-0-95-final.dmg" class="screeny"><img class="thumb" src="images/download-macosx.png" alt="Screenshot" /></a><br />Mac OS X</li>
             <li><a href="http://sourceforge.net/project/showfiles.php?group_id=54091" class="screeny"><img class="thumb" src="images/download-other.png" alt="Screenshot" /></a><br />Other Packages</li>
-            <li><hr width=200> <a href="http://amsn.sourceforge.net/amsn_dev.tar.gz" class="screeny"><img class="thumb" src="images/download-svn.png" alt="Screenshot" /></a><br />Latest development version (SVN Snapshot <?php if (file_exists('amsn_dev.tar.gz')) { echo 'of '.date("F d Y H:i:s.", filectime('amsn_dev.tar.gz')); } ?> )</li>
+            <li><hr style="width:200px;" /> <a href="http://amsn.sourceforge.net/amsn_dev.tar.gz" class="screeny"><img class="thumb" src="images/download-svn.png" alt="Screenshot" /></a><br />Latest development version (SVN Snapshot <?php if (file_exists('amsn_dev.tar.gz')) { echo 'of '.date("F d Y H:i:s.", filectime('amsn_dev.tar.gz')); } ?> )</li>
           </ul>
    </div>
 
