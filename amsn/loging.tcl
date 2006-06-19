ed09eb4e4a6e50e523f0076de48bc97c2a096175
jonne_z <jonne_z@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-06-19 18:46:12 +0000

    [.main.eventmenu.list index end] does not give the number of elements in the list correctly.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@6948 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/loging.tcl
+++ amsn/loging.tcl
@@ -1458,7 +1458,7 @@ namespace eval ::log {
 		if {[::config::getKey display_event_$event]} {
 			set eventmenu "[clock format [clock seconds] -format "%H:%M:%S"] : $eventlog"
 			.main.eventmenu.list list insert 0 $eventmenu
-			if { [.main.eventmenu.list list index end] > 100 } {
+			if { [.main.eventmenu.list list size] > 100 } {
 				.main.eventmenu.list list delete 100 end
 			}
 			.main.eventmenu.list select 0
