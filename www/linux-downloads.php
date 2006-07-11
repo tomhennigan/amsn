3c34173e74ef975bf2cc223ffac2a908f413ebe0
tjikkun <tjikkun@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-07-11 21:02:57 +0000

    0.96rc1
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7037 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/linux-downloads.php
+++ www/linux-downloads.php
@@ -17,20 +17,17 @@ Please select your desired package from one of the drop-down menus:
 <p>
 <select class="dlselector" name="file" onchange="this.form.submit()">
 <option value="" selected="selected" />
-<option value="amsn-0.95-2.linux-installer.bin">Bitrock Installer</option>
-<option value="amsn-0.95-2.x86.package">AutoPackage</option>
-<option value="amsn_0.95-3.deb">Debian</option>
-<option value="amsn_0.95-1.sarge.deb">Debian (Sarge)</option>
-<option value="amsn_0.95-3.ubuntu.deb">Ubuntu</option>
-<option value="amsn_0.95-ubuntu.powerpc.deb">Ubuntu (PowerPC)</option>
-<option value="amsn-0.95-3.fc3.i686.rpm">Fedora Core 3</option>
-<option value="amsn-0.95-3.fc4.i686.rpm">Fedora Core 4</option>
-<option value="amsn-0.95-1-mdk92.i686.rpm">Mandrake 9.2</option>
-<option value="amsn-0.95-3mdk.i686.rpm">Mandriva 2006 (Formerly Mandrake)</option>
-<option value="amsn-0.95-i486.tgz">Slackware</option>
-<option value="amsn-0.95-2.suse.i586.rpm">SUSE 10.0</option>
-<option value="amsn-0.95-1.archlinux.tar.gz">Archlinux</option>
-<option value="amsn-0.95.tar.gz">Other</option>
+<option value="amsn-0.96RC1-linux-installer.bin">Bitrock Installer</option>
+<option value="amsn-0.96RC1.x86.package">AutoPackage</option>
+<option value="amsn_0.96rc1-1.i386.stable.deb">Debian</option>
+<option value="amsn_0.96rc1-1.i386.testing.deb">Debian (Sarge)</option>
+<option value="amsn_0.96RC1-1.deb">Ubuntu</option>
+<option value="amsn_0.96rc1-PPC.deb">Ubuntu (PowerPC)</option>
+<option value="amsn-0.96RC1-1.fc4.i686.rpm">Fedora Core 4</option>
+<option value="amsn-0.96RC1-1-mandriva.i686.rpm">Mandriva 2006 (Formerly Mandrake)</option>
+<option value="amsn-0.96RC1-1-suse.i686.rpm">SUSE 10.0</option>
+<option value="amsn-0.96RC1-1.archlinux.pkg.tar.gz">Archlinux</option>
+<option value="amsn-0.96RC1-i686.tgz">Other</option>
 </select>
 <input type="submit" value="Download"/>
 </p>
