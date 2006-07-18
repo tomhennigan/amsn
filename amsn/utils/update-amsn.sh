223c52689cc63c5e99cb44f372cdeac456633270
airadier <airadier@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-07-18 19:07:30 +0000

    Updated update-amsn.sh script, thanks to Rafael Rodriguez
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7058 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/update-amsn.sh
+++ amsn/utils/update-amsn.sh
@@ -8,39 +8,16 @@
 #v0.1: 18/01/03
 ##############################################################################
 
-AMSNPATH=~/msn
-EXTRASPATH=~/msn/amsn-extras
+AMSNPATH=~/amsn
+EXTRASPATH=~/amsn/amsn-extras
 
 cd ${AMSNPATH}
 echo "Updating amsn..."
-cvs -f -q update -d -P "."
-cd ${EXTRASPATH}
-echo "Updating amsn-extras..."
-cvs -f -q update -d -P "."
+svn update
+if [ -d ${EXTRASPATH} ]; then
+	cd ${EXTRASPATH}
+	echo "Updating amsn-extras..."
+	svn update
+fi
 
-cd ${EXTRASPATH}/skins
-for SKIN in *
-do
-	if [ $SKIN != "CVS" ]; then
-		if [ ! -e "${AMSNPATH}/skins/${SKIN}" ]; then	#Link already exists?
-			echo "Linking skin ${SKIN}..."
-			ln -s ${EXTRASPATH}/skins/${SKIN} ${AMSNPATH}/skins/${SKIN}
-		else
-			echo "Not linking skin ${SKIN}. Link already exists..."
-		fi
-	fi
-done
-		
-cd ${EXTRASPATH}/plugins
-for PLUGIN in *
-do
-	if [ $PLUGIN != "CVS" ]; then
-		if [ ! -e "${AMSNPATH}/plugins/${PLUGIN}" ]; then	#Link already exists?
-			echo "Linking plugin ${PLUGIN}..."
-			ln -s ${EXTRASPATH}/plugins/${PLUGIN} ${AMSNPATH}/plugins/${PLUGIN}
-		else
-			echo "Not linking plugin ${PLUGIN}. Link already exists..."
-		fi
-	fi
-done
-		
+
