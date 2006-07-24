7035c44f9bb9062ce3a43cab59c6bc608d09d8c8
lephilousophe <lephilousophe@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-07-24 21:40:26 +0000

    Added more platforms (I fixed the corrupted file on shell server)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7080 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/linux-downloads.php
+++ www/linux-downloads.php
@@ -21,6 +21,7 @@ Please select your desired package from one of the drop-down menus:
 <option value="amsn-0.96RC1.x86.package">AutoPackage</option>
 <option value="amsn_0.96rc1-1.i386.stable.deb">Debian</option>
 <option value="amsn_0.96rc1-1.i386.testing.deb">Debian (Sarge)</option>
+<option value="amsn_0.96RC1-1kubuntu1_i386.deb">KUbuntu</option>
 <option value="amsn_0.96RC1-1.deb">Ubuntu</option>
 <option value="amsn_0.96rc1-PPC.deb">Ubuntu (PowerPC)</option>
 <option value="amsn-0.96RC1-1.fc4.i686.rpm">Fedora Core 4</option>
@@ -28,6 +29,7 @@ Please select your desired package from one of the drop-down menus:
 <option value="amsn-0.96RC1-i686.tgz">Slackware</option>
 <option value="amsn-0.96RC1-1-suse.i686.rpm">SUSE 10.0</option>
 <option value="amsn-0.96RC1-1.archlinux.pkg.tar.gz">Archlinux</option>
+<option value="amsn-0.96_rc1.ebuild">Gentoo</option>
 <option value="amsn-0.96RC1.tar.gz">Other</option>
 </select>
 <input type="submit" value="Download"/>
