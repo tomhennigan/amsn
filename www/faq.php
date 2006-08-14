88129bfd6a2853d93d0c85b0a322ec074bc2741e
tjikkun <tjikkun@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-08-14 14:13:12 +0000

    I think it should point there, correct me if i am wrong
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7129 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/faq.php
+++ www/faq.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: http://amsn.sourceforge.net/wiki/tiki-index.php?page=Frequently+Asked+Questions");
+header("Location: http://amsn.sourceforge.net/userwiki/index.php/Frequently_Asked_Questions");
 ?>
 
 <!DOCTYPE html
@@ -10,6 +10,6 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 <title>Redirect...</title>
 </head>
 <body>
-Your web browser should have redirected you automaticly. If not, please <a href="http://amsn.sourceforge.net/modules.php?name=Skins">click here</a>.
+Your web browser should have redirected you automaticly. If not, please <a href="http://amsn.sourceforge.net/userwiki/index.php/Frequently_Asked_Questions">click here</a>.
 </body>
 </html>
