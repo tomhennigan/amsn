6e1110f5ada826d277fc41460ac28a0f79faa84f
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-08-23 11:59:10 +0000

    Committing a comment from a fresh directory, just in case SF understands that the damn merge failed and it will never work instead of throwing error 500: commit failed
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7154 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/chatwindow.tcl
+++ amsn/chatwindow.tcl
@@ -873,6 +873,8 @@ namespace eval ::ChatWindow {
 		pack $paned -side top -expand true -fill both -padx [::skin::getKey chat_paned_padx]\
 		 -pady [::skin::getKey chat_paned_pady]
 
+		# Tabbed chatwindows have focus by themselves, the focus thing
+		# seems to mess them up
 		if { [::config::getKey tabbedchat] != 0 } { 
 			focus $paned
 		}
