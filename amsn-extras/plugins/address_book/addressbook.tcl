adc2dd42a0f6c37057d7b030c214563c11fe42ea
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-09-24 20:32:11 +0000

    AddressBook Plugin Final 0.1 version. Auto-update *should* be working, and I added some more catch statements to the osascript execs. :-)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7304 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/address_book/addressbook.tcl
+++ amsn-extras/plugins/address_book/addressbook.tcl
@@ -6,7 +6,7 @@
 #
 
 namespace eval ::macabook {
-	proc macabookInit { dir } {
+	proc init { dir } {
 		# Register the plugin.
 		::plugins::RegisterPlugin "Address Book"
 
@@ -14,33 +14,39 @@ namespace eval ::macabook {
 		set ::macabook::config(path) $dir
 		
 		# Set our state to 1 (AddressBook names override Custom & Nick names).
-		set ::macabook::config(macabookstate) 1
+		set ::macabook::config(state) 1
 		
 		# Bind our key stroke (Command-Shift-a) to switch the state of names in the CL.
 		bind all <Command-A> "::macabook::switchState"
 		
 		# Load a list of all people with MSN handles
-		set ::macabook::config(macabookusableaddresses) [exec osascript [file join $dir addressbook.applescript] returnAllMSNAddresses]
+		set error1 [catch { set ::macabook::config(usableaddresses) [exec osascript [file join $dir addressbook.applescript] returnAllMSNAddresses] }]
 		
 		# Cache nicks.
-		set ::macabook::config(macabookcachednicks) [exec osascript [file join $dir addressbook.applescript] returnAllCustomNicks]
+		set error2 [catch { set ::macabook::config(cachednicks) [exec osascript [file join $dir addressbook.applescript] returnAllCustomNicks] }]
 		
 		# Quit Address Book
-		catch { exec killall Address\ Book }
+		set error3 [catch { exec killall Address\ Book }]
+			
+		# Check for errors.
+		if { $error1 || $error2 || $error3 } {
+			status_log "Error with osascript command. The log from the first command is: $error1.\nThe second log is: $error2.\nThe third log is: $error3." red
+		}
 		
 		# Register for our post event.
 		::plugins::RegisterEvent "Address Book" getDisplayNick parse_nick
 	}
 
 	# The abook deinit proc.
-	proc macabookDeInit { } {
+	proc deinit { } {
 		# Unbind our key-stroke.
 		bind all <Command-A> ""
+		test
 	}
 
 	# This is called when the post event occurs.
 	proc parse_nick {event epvar} {
-		if {$::macabook::config(macabookstate)} {
+		if {$::macabook::config(state)} {
 			# Get the pointer to the variables from the proc, so we can send feedback.
 			upvar 2 user_login user_login customnick customnick
 		
@@ -54,7 +60,7 @@ namespace eval ::macabook {
 	}
 	
 	proc checkLogin { user_login } {
-		if {[lsearch $::macabook::config(macabookusableaddresses) $user_login] != "-1"} {
+		if {[lsearch $::macabook::config(usableaddresses) $user_login] != "-1"} {
 			return 1
 		} else {
 			return 0
@@ -63,15 +69,15 @@ namespace eval ::macabook {
 	
 	proc getABookName { user_login } {
 		# Return the users name from address book.
-		return [lindex $::macabook::config(macabookcachednicks) [lsearch $::macabook::config(macabookusableaddresses) $user_login]]
+		return [lindex $::macabook::config(cachednicks) [lsearch $::macabook::config(usableaddresses) $user_login]]
 	}
 	
 	proc switchState { } {
 		# A logic switch to change between Address Book Mode and aMSN Mode.
-		if {$::macabook::config(macabookstate)} {
-			set ::macabook::config(macabookstate) 0
+		if {$::macabook::config(state)} {
+			set ::macabook::config(state) 0
 		} else {
-			set ::macabook::config(macabookstate) 1
+			set ::macabook::config(state) 1
 		}
 		cmsn_draw_online
 	}
