3414ff648fde881f263390cf71e28a8963af24f3
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-10-15 21:29:15 +0000

    This should make the siren_test compatible with big endian platforms... (partially)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7358 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/tcl_siren/src/siren_test.c
+++ amsn/utils/tcl_siren/src/siren_test.c
@@ -34,6 +34,13 @@ typedef struct {
 } fmtChunkEx;
 
 
+#define IDX(val, i) ((unsigned int) ((unsigned char *) &val)[i])
+
+#define GUINT16_FROM_LE(val) ( (unsigned short) ( IDX(val, 0) + (unsigned short) IDX(val, 1) * 256 ))
+#define GUINT32_FROM_LE(val) ( (unsigned int) (IDX(val, 0) + IDX(val, 1) * 256 + \
+        IDX(val, 2) * 65536 + IDX(val, 3) * 16777216)) 
+
+
 
 int main(int argc, char *argv[]) {
 	FILE * input;
@@ -72,16 +79,24 @@ int main(int argc, char *argv[]) {
 	fread(&riff_header, sizeof(RIFF), 1, input);
 	fileOffset += sizeof(RIFF);
 
+	riff_header.ChunkId = GUINT32_FROM_LE(riff_header.ChunkId);
+	riff_header.ChunkSize = GUINT32_FROM_LE(riff_header.ChunkSize);
+	riff_header.TypeID = GUINT32_FROM_LE(riff_header.TypeID);
+
 	if (riff_header.ChunkId == RIFF_ID && riff_header.TypeID == WAVE_ID) {
 		while (fileOffset < riff_header.ChunkSize) {
 			fread(&current_chunk, sizeof(WAVE_CHUNK), 1, input);
 			fileOffset += sizeof(WAVE_CHUNK);
+			current_chunk.ChunkId = GUINT32_FROM_LE(current_chunk.ChunkId);
+			current_chunk.ChunkSize = GUINT32_FROM_LE(current_chunk.ChunkSize);
 
 			chunkOffset = 0;
 			if (current_chunk.ChunkId == FMT__ID) {
 				fread(&fmt_info, sizeof(fmtChunk), 1, input);
+				/* Should convert from LE the fmt_info structure, but it's not necessary... */
 				if (current_chunk.ChunkSize > sizeof(fmtChunk)) {
 					fread(&(fmt_info.ExtraSize), sizeof(short), 1, input);
+					fmt_info.ExtraSize= GUINT32_FROM_LE(fmt_info.ExtraSize);
 					fmt_info.ExtraContent = (unsigned char *) malloc (fmt_info.ExtraSize);
 					fread(fmt_info.ExtraContent, fmt_info.ExtraSize, 1, input);
 				} else {
@@ -105,6 +120,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	
+	/* The WAV heder should be converted TO LE, but should be done inside the library and it's not important for now ... */
 	fwrite(&(encoder->WavHeader), sizeof(encoder->WavHeader), 1, output);
 	fwrite(out_data, 1, encoder->WavHeader.DataSize, output);
 	fclose(output);
