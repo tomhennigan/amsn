fa246c0157c7b3813e6bb7d199a19969acc1e0a2
lephilousophe <lephilousophe@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-10-15 21:09:28 +0000

    I just remind : snack is already provided with the NSIS installer...
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7357 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/windows/snack2.2/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-#
-# Immediate loading of Snack because of item types etc.
-#
-# http://www.wjduquette.com/tcl/namespaces.html
-#
-
-package ifneeded snack 2.2.10 "[list load [file join $dir libsnack.dll]];[list source [file join $dir snack.tcl]]"
-
-package ifneeded sound 2.2.10 [list load [file join $dir libsound.dll]]
