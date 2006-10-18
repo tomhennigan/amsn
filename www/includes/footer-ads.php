8bd39a692a7cc32f5e9166fa9e1cb3273f41541f
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-10-18 17:37:36 +0000

    changed text+image to a text only ad...
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7369 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/includes/footer-ads.php
+++ www/includes/footer-ads.php
@@ -4,7 +4,7 @@ google_ad_client = "pub-4657753156267954";
 google_ad_width = 728;
 google_ad_height = 90;
 google_ad_format = "728x90_as";
-google_ad_type = "text_image";
+google_ad_type = "text";
 //2006-10-03: amsn
 google_ad_channel ="7037776257";
 //--></script>
