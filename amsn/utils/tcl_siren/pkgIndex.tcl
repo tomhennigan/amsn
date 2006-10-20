e27c412d62c72a502c37b4729316c6f40fecdad2
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-10-20 00:01:15 +0000

    Incremented version (because of Mac support) and added Mac .dylib (until Tom/J uploads a 'certified' dylib)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7373 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/tcl_siren/pkgIndex.tcl
+++ amsn/utils/tcl_siren/pkgIndex.tcl
@@ -1,5 +1,5 @@
-# Tcl package index file, version 1.0
+# Tcl package index file
 
 if {[package vcompare [info tclversion] 8.4] < 0} return
 
-package ifneeded tcl_siren 0.1 "[list load [file join $dir tcl_siren[info shared]] Siren];package provide tcl_siren 0.1"
+package ifneeded tcl_siren 0.2 "[list load [file join $dir tcl_siren[info shared]] Siren];package provide tcl_siren 0.2"
