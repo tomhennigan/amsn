29a7388721d78e4e29d3b1da1dcc9ba8f5d60da0
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-10-22 01:05:49 +0000

    Added libsnack 2.2.10 for mac users...
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7391 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/utils/macosx/snack2.2/pkgIndex.tcl
@@ -0,0 +1,9 @@
+#
+# Immediate loading of Snack because of item types etc.
+#
+# http://www.wjduquette.com/tcl/namespaces.html
+#
+
+package ifneeded snack 2.2.10 "[list load [file join $dir libsnack.dylib]];[list source [file join $dir snack.tcl]]"
+
+package ifneeded sound 2.2.10 [list load [file join $dir libsound.dylib]]
