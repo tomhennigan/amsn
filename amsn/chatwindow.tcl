84d1a3c11eb6769c7b9bcf5c518402f62508d7bc
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-10-22 09:01:25 +0000

    FINALLY it works for Mac!!!!! the [$sound datasamples] returned the samples in native endianness, so it was already big endian for Mac, and tcl_siren expected it to be always little endian (because of wav files format)... now I found the -byteorder option for the datasamples command, thank god snack is advanced enough to allow us to choose the endianness in our samples (too bad the datasamples command is undocumented as well as the -byteorder option and I had to look in snack's source code to find them!) another issue is that when you have snack 2.2.x and 2.2.10 (from amsn utils/macosx) installed, the package require fails because of 'conflicting versions' instead of choosing the latest version (2.2.10) like on windows or linux.. this is now fixed! Thanks Tom for the mac access, the feedback and the testing!
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7393 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/chatwindow.tcl
+++ amsn/chatwindow.tcl
@@ -2229,13 +2229,26 @@ namespace eval ::ChatWindow {
 			::CAMGUI::WebcamWizard
 		}
 	}
+
+	# This needs to be done this way because it seems Mac shows an error about conflicting versions if you have 2.2 and 2.2.10 installed...
+	# but on linux (and windows) it takes the latest version (2.2.10) without complaining...
+	# Since Mac and Windows have the snack 2.2.10 binaries shipped with amsn,we can require the exact version safely.. linux is trickier because
+	# the user can install whatever version he wants (and all 2.2.x versions have a 'package provide 2.2')
+	proc require_snack { } {
+		if {[OnLinux] } {
+			package require snack
+		} else {
+			package require snack 2.2.10
+		}
+	}
+
 	proc start_voice_clip { w } {
 		variable voice_sound
 		variable voice_text_pack
 
 		set chatid [Name $w]
 		if { [llength  [::MSN::usersInChat $chatid]] > 0 } {
-			if { [catch { package require snack } ] || [catch {package require tcl_siren }] } {
+			if { [catch {require_snack} ] || [catch {package require tcl_siren }] } {
 				amsn::WinWrite $chatid "\n" red
 				amsn::WinWriteIcon $chatid greyline 3
 				amsn::WinWrite $chatid "\n" red
@@ -2398,7 +2411,9 @@ namespace eval ::ChatWindow {
 				amsn::WinWrite $chatid "[timestamp] [trans nosound]\n" red
 			} else {
 				set enc [::Siren::NewEncoder]
-				if { [catch {set out [::Siren::Encode $enc [$voice_sound datasamples]] } res] } {
+				# We need to add the -byteorder littleEndian because on Mac, the datasamples are in big endian and tcl_siren expects little endian
+				# since WAV file contents are in little endian.
+				if { [catch {set out [::Siren::Encode $enc [$voice_sound datasamples -byteorder littleEndian]] } res] } {
 					::Siren::Close $enc    		
 					status_log "Error Encoding voice clip to Siren codec : $res" red
 					return 0
