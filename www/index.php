59bf05104d8aee40a6980d670d89084d944933b4
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-11-30 02:09:58 +0000

    updating main site link... we should make this automatic, like dlfile.php?os= instead of file=, and only change the occurences in dlfile.php
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7547 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/index.php
+++ www/index.php
@@ -42,7 +42,7 @@ echo '<br /><br />';
 
 switch(remoteOS()) {
     case 'Windows':
-    $url='dlfile.php?file=aMSN-0.96-windows-installer.exe';
+    $url='dlfile.php?file=aMSN-0.96-3-windows-installer.exe';
     break;
     case 'Linux':
     $url='linux-downloads.php';
