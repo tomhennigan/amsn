97add30b36dd4f5acc0914ffe091f8e18d485ce0
tjikkun <tjikkun@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2006-12-31 13:22:01 +0000

    api change so version update is needed
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7713 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/linux/capture/pkgIndex.tcl
+++ amsn/utils/linux/capture/pkgIndex.tcl
@@ -2,4 +2,4 @@
 
 if {[package vcompare [info tclversion] 8.4] < 0} return
 
-package ifneeded capture 0.4 "package require Tk; [list load [file join $dir capture.so] capture]; package provide capture 0.4"
+package ifneeded capture 0.5 "package require Tk; [list load [file join $dir capture.so] capture]; package provide capture 0.5"
