a40e54997ff8e930386280e1e40d5859d1a80a62
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-01-08 11:25:26 +0000

    Custom Display Pictures from Cristofaro + added 2 missing lang keys (I couldnt find any more keys???)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7725 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/dpbrowser/dpbrowser.tcl
+++ amsn/utils/dpbrowser/dpbrowser.tcl
@@ -7,8 +7,15 @@ snit::widgetadaptor dpbrowser {
 	option -width -default 5
 #	option -height -default 500
 	option -bg -default white -configuremethod SetConfig
+	option -bg_hl -default DarkBlue -configuremethod SetConfig
 	option -mode -default "properties"
 	#modes "properties" where you right-click with actions and mode "select" where left click sets as your image preview for new pic browser"
+	
+	# When using select mode, it's important to pass the name of a procedure through this option.
+	# Otherwise, the parent window will not react to the change of selection.
+	option -post_select -default ""
+	option -picinuse -default "no" -configuremethod SetConfig
+	option -disableselect -default "no"
 
 #	option -addinuse -default 0 -configuremethod SetConfig
 
@@ -55,7 +62,7 @@ puts "sw created"
 
 		if {$email == "self"} {
 			set cachefiles [glob -nocomplain -directory [file join $HOME displaypic] *.dat]
-			set pic_in_use [::abook::getContactData $email displaypicfile ""]
+			set pic_in_use ""
 		} else {
 			set cachefiles [glob -nocomplain -directory [file join $HOME displaypic cache] *.dat]	
 			set pic_in_use [::abook::getContactData $email displaypicfile ""]
@@ -75,7 +82,7 @@ puts "sw created"
 		
 			foreach file $cachefiles {
 				#exclude the image the user is currently using
-				if { [string first $pic_in_use $file] == -1 } {
+				if { $options(-picinuse) != "no" || [string first $pic_in_use $file] == -1 } {
 				set fd [open $file]
 
 
@@ -91,25 +98,19 @@ puts "sw created"
 					if { [catch { image create photo userDP_${email}_$i -file [filenoext $file].png -format cximage }] } { continue }
 					::picture::ResizeWithRatio userDP_${email}_$i 96 96
 					set entry $frame.${i}_shell
-					frame $entry -bg $color -bd 0 -relief flat
+					frame $entry -bg $color -bd 2 -relief flat
 
 					label $entry.img -image userDP_${email}_$i -bg $color
 					bind $entry <Destroy> "catch { image delete userDP_${email}_$i}"
-					if {$email == "self"} {
-						bind $entry.img <ButtonPress-3> \
-							[list $self dp_popup_menu %X %Y\
-							[filenoext $file].png $entry.img $email]
-						#selection binding
-						bind $entry.img <ButtonPress-1> [list $self selectdp [filenoext $file].png $entry.img]
-					} else {
-						bind $entry.img <ButtonPress-3> \
-							[list $self dp_popup_menu %X %Y\
-							[filenoext $file].png $entry.img $email]
-					}
+					bind $entry.img <ButtonPress-3> \
+						[list $self dp_popup_menu %X %Y\
+						[filenoext $file].png $entry.img $email]
+					#selection binding
+					bind $entry.img <ButtonPress-1> [list $self selectdp [filenoext $file].png $entry.img]
 
 	#TODO: a tooltip with the full size image
-					bind $entry.img <Enter> ""
-					bind $entry.img <Leave> ""
+#					bind $entry.img <Enter> "showtooltip %X %Y [filenoext $file].png"
+#					bind $entry.img <Leave> "showtooltip %X %Y [filenoext $file].png"
 
 					label $entry.text -text [lindex $greps 1] -bg $color
 
@@ -181,7 +182,7 @@ puts "sw created"
 			-label "[trans copytoclipboard [string tolower [trans filename]]]" \
 			-command [list clipboard clear ; clipboard append $filename]
 		$the_menu add command -label "[trans delete]" \
-			-command [list pictureDeleteFile $filename $widget]
+			-command [list $self deleteentry $filename $widget]
 #		$the_menu add command -label "Set as custom display picture for this user" \
 			-command [list ::amsn::messageBox "Sorry, not yet implemented" ok error [trans failed]]
 		$the_menu add command -label "[trans setasmydp]" \
@@ -190,14 +191,46 @@ puts "sw created"
 	}
 
 	method selectdp { file imgwidget } {
-		global selected_path
-		if { [catch { image create photo my_pic -file $file -format cximage }] } { return }
-		set selected_path $file
-		#select the image in the widget ?
+		if {$options(-disableselect) == "no" } {
+			global selected_path
+			if { [catch { image create photo my_pic -file $file -format cximage }] } { return }
+			set selected_path $file
+			# Highlight only the selected image
+			for {set i 0} {[winfo exists [$self.sw.sf getframe].${i}_shell]} {incr i} {
+				set entry [$self.sw.sf getframe].${i}_shell
+				set entry_img $entry.img
+				if { $entry_img != $imgwidget } {				
+					$entry configure -background $options(-bg)
+				} else {
+					$entry configure -background $options(-bg_hl)
+				}
+			}
+			# Execute the post-select procedure, sending the browser window as parameter
+			if { $options(-post_select) != "" } {
+				eval $options(-post_select) $self
+			}
+		}
+	}
+
+	method unselect_all { } {
+		for {set i 0} {[winfo exists [$self.sw.sf getframe].${i}_shell]} {incr i} {
+			set entry [$self.sw.sf getframe].${i}_shell
+			$entry configure -background $options(-bg)
+		}
+
 	}
 	
 	method getselection {} {
 		global selected_path
 		return $selected_path
+	}
+
+	method deleteentry {filename widget} {
+		global selected_path
+		if {$selected_path == $filename} {
+			set selected_path ""
+		}
+		pictureDeleteFile $filename $widget
+		$self fill
 	}
 }
