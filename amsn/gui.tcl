336608448374a811afcf564c7335ec2414e83bb5
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-01-17 14:36:15 +0000

    why was that commented?
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7791 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/gui.tcl
+++ amsn/gui.tcl
@@ -3563,7 +3563,7 @@ proc cmsn_draw_main {} {
 
 	#Draw main window contents
 	cmsn_draw_status
-#	cmsn_draw_offline
+	cmsn_draw_offline
 
 	if { [OnWin] } {
 		catch {wm iconbitmap . [::skin::GetSkinFile winicons msn.ico]}
