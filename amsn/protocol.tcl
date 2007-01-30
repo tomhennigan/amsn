448f46fcf60d4101e558d81dc83c0fcc92facd23
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-01-30 19:38:37 +0000

    Youness seems to have fixed the issue with Authentication Failed with some nicknames when sending a PSM. Dunno yet, i tried once and it worked (it didnt use to work!). Youness did this, I just tested and I am comitting because I had the issue...
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7865 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/protocol.tcl
+++ amsn/protocol.tcl
@@ -3105,7 +3105,9 @@ namespace eval ::MSNOIM {
 			set seq_number 1
 		}
 		set bmessage [base64::encode [encoding convertto identity $msg]]
-		set bnick [base64::encode [encoding convertto identity [::abook::getPersonal MFN]]]
+		#set bnick [base64::encode [encoding convertto identity [::abook::getPersonal MFN]]]
+
+		set bnick [string map {"\n" "" } [base64::encode [encoding convertto utf-8 [::abook::getPersonal MFN]]]]
 
 		set ticket [string map { "&" "&amp;" } $ticket]
 		set xml {<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Header><From}
