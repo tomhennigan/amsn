56133b59f243bc652f7e83696357f1b962be5710
lephilousophe <lephilousophe@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-02-04 21:24:12 +0000

    Added the script to update priorities Grabbed from roadkillbunny's space on SF
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7923 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ www/bugs/admin/prioritize.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+<?php
+include('../../config.inc.php');
+include('../../bugs/common.inc.php');
+
+$reports=array();
+$max=0;
+
+$query="SELECT * FROM ".TBUGS." WHERE bug_flag='0'";
+$result=mysql_query($query) or die("MySQL Error: ".mysql_error());
+
+while($row=mysql_fetch_array($result)) {
+  $id=$row['bug_id'];
+  $query="SELECT COUNT(*) as num FROM ".TBUGREPORTS." WHERE bug_parent='$id'";
+  $result1=mysql_query($query) or die("MySQL Error: ".mysql_error());
+  $row1=mysql_fetch_array($result1);
+  $reports[$id]=$row1['num'];
+  if($row1['num']>$max)
+    $max=$row1['num'];
+}
+
+foreach($reports as $id => $count) {
+  $priority=intval($count/$max*100);
+  echo 'Bug #'.$id.' has priority '.$priority."\n";
+  $query="UPDATE ".TBUGS." SET bug_priority='".$priority."' WHERE bug_id='".$id."'";
+  mysql_query($query) or die("MySQL Error: ".mysql_error());
+}
+
+?>
+</body>
+</html>
