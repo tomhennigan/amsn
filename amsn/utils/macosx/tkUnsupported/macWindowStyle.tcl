0b931b4e9ebf76e0af1aceb05b0424e19bd4d029
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-02-08 12:11:47 +0000

    The transparent plugin will now handle transparency.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@7940 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/tkUnsupported/macWindowStyle.tcl
+++ amsn/utils/macosx/tkUnsupported/macWindowStyle.tcl
@@ -9,22 +9,7 @@ rename toplevel Tk_toplevel
 proc toplevel { pathname args } {
 	set window [eval Tk_toplevel \"$pathname\" $args]
 	setBrushed $window
-	setAlpha $window "0.9"
 	return $window
-}
-
-proc setAlpha { window alpha } {
-	if {$window != ".balloon"} {
-		# Balloons don't work with alpha values. This needs to be set in balloon.tcl when the window is made, after the tkunsupported command.
-		
-		# I know this looks weird and wrong, but this is called before config.tcl is loaded.
-		set setAlpha "0"
-		catch {set setAlpha [::config::isSet windowalpha]}
-		
-		if {$setAlpha} {
-			catch {wm attributes $window -alpha [::config::getKey windowalpha "1.0"]}
-		}
-	}
 }
 
 proc setBrushed {window} {
