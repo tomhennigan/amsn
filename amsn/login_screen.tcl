a0fba7c49692d2134ae68567dc026e9d0e551e93
bluetit <bluetit@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-03-12 14:59:48 +0000

    Removed intial size-setting of input fields. Now handled by Resize method
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@8200 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/login_screen.tcl
+++ amsn/login_screen.tcl
@@ -170,12 +170,6 @@ snit::widgetadaptor loginscreen {
 			}
 		}
 
-		# Make all text input fields equal widths
-		set field_width [font measure splainf -displayof $self "__________________________"]
-		$self itemconfigure $user_field_tag -width $field_width
-		$self itemconfigure $pass_field_tag -width $field_width
-		$self itemconfigure $status_field_tag -width $field_width
-
 		# Make checkbuttons look nice
 		foreach checkbutton [list $rem_me_field $rem_pass_field $auto_login_field] {
 			$checkbutton configure -relief flat -highlightthickness 0 -bd 0 -bg white -selectcolor white -image [::skin::loadPixmap checkbox] -selectimage [::skin::loadPixmap checkbox_on] -indicatoron 0
