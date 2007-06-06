b6e56d7501ca4191b3c64c6ffa3ce5ec9a38edbd
lephilousophe <lephilousophe@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-06-06 15:29:58 +0000

    Revert back : pigdin is immune with that as they use frames...
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@8841 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/common.php
+++ www/common.php
@@ -14,16 +14,5 @@ if(isset($_GET['lang'])) {
   setLangKey($_GET['lang']);
 }
 
-/*
-	NoWhereMan: redirects people from piGdin.im to  www.pidgin.im
-*/
-if(isset($_SERVER['HTTP_HOST']) && 
-	strpos($_SERVER['HTTP_HOST'], 'pigdin')!==false) {
-	header( "HTTP/1.1 301 Moved Permanently" );
-	header( "Location: http://www.pidgin.im" ); 
-	exit();
-}
-/* end of redirect */
-
 header('Content-type: text/html;charset=utf-8');
 ?>
