a26ca7ca5c6684a7ba029d4f44975ca2f9215b79
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-12-05 23:11:23 +0000

    Brushed metal is broken on Mac OS X 10.5, so we temporarily disable it for this version.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@9106 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/tkUnsupported/macWindowStyle.tcl
+++ amsn/utils/macosx/tkUnsupported/macWindowStyle.tcl
@@ -13,8 +13,21 @@ proc toplevel { pathname args } {
 }
 
 namespace eval macWindowStyle {
+	# Cached variable storing the system version IE. 10.3.9, 10.4.10 or 10.5.1
+	variable macos_version [exec sw_vers -productVersion]
+	
 	proc setBrushed {window} {
+		variable macos_version
+		# Brushed style windows are buggy with 10.5, so we diable them for 10.5+
+		set mac_ver_major [lindex [version_intList $macos_version] 0]
+		set mac_ver_minor [lindex [version_intList $macos_version] 1]
+		
 		if {[::skin::getKey usebrushedmetal "0"] && [::config::getKey allowbrushedmetal "1"]} {
+			if {$mac_ver_major >= 10 && $mac_ver_minor >= 5} {
+				status_log "::macWindowStyle::setBrushed - style disabled for os version ($macos_version)" white
+				return
+			}
+			
 	    	catch {
 	    		::tk::unsupported::MacWindowStyle style $window document {closeBox horizontalZoom verticalZoom collapseBox resizable metal}
 	    	}
