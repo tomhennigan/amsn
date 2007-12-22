294b2024ddfd3d5ba8d8407d39f9a5b58e8c6e29
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2007-12-22 19:27:31 +0000

    minor change. comment separate from prototype line
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@9145 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/tcl_siren/src/encoder.h
+++ amsn/utils/tcl_siren/src/encoder.h
@@ -16,8 +16,8 @@ typedef struct stSirenEncoder {
 	float context[320];
 } * SirenEncoder;
 
-
-extern SirenEncoder Siren7_NewEncoder(int sample_rate); /* MUST be 16000 to be compatible with MSN Voice clips (I think) */
+/* sample_rate MUST be 16000 to be compatible with MSN Voice clips (I think) */
+extern SirenEncoder Siren7_NewEncoder(int sample_rate);
 extern void Siren7_CloseEncoder(SirenEncoder encoder);
 extern int Siren7_EncodeFrame(SirenEncoder encoder, unsigned char *DataIn, unsigned char *DataOut);
 
