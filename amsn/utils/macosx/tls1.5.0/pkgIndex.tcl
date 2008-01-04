20d0e3914944ac789eb4934d6889d6a8f350e8f5
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-01-04 16:11:42 +0000

    Change tls package version from 1.50 to 1.5.0 on macosx
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@9225 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/tls1.5.0/pkgIndex.tcl
+++ amsn/utils/macosx/tls1.5.0/pkgIndex.tcl
@@ -5,5 +5,5 @@
 #    replace the original which didn't include the commands from "tls.tcl".
 #
 
-package ifneeded tls 1.50 "[list load [file join $dir libtls1.50.dylib] ] ; [list source [file join $dir tls.tcl] ]"
+package ifneeded tls 1.5.0 "[list load [file join $dir libtls1.5.0.dylib] ] ; [list source [file join $dir tls.tcl] ]"
 
