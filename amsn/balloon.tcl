4e1714c00ff5636058db18c634530de360b1adce
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-01-06 00:21:25 +0000

    Fix bug #110. The bundled version of Tk (8.4.7) doesn't support -alpha.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@9252 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/balloon.tcl
+++ amsn/balloon.tcl
@@ -220,7 +220,7 @@ proc balloon {target message pic {cx 0} {cy 0} {fonts ""} {mode "simple"} } {
 		if { [OnMac] } {
 			#Set alpha value of balloon. This is done after setting the geometry to
 			#avoid the balloon to be placed at a random position on screen.
-			wm attributes .balloon -alpha [::skin::getKey balloonalpha 0.9]
+			catch { wm attributes .balloon -alpha [::skin::getKey balloonalpha 0.9] }
 
 			#Focus last windows in AquaTK (to fix "Mac OS X focus bug")
 			if { $lastfocus!="" } {
