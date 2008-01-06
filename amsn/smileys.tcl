7c775554f950d0c6e0474c1097ae5e7ec909c168
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-01-06 13:40:20 +0000

    Fix issue where { and } weren't escaped with smileys. Fixes bug #135
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@9281 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/smileys.tcl
+++ amsn/smileys.tcl
@@ -1281,7 +1281,7 @@ proc custom_smile_subst { chatid tw {textbegin "0.0"} {end "end"} } {
 			set copyMenu "${tw}.emoticonCustom_std_$file"
 			if { ![winfo exists $copyMenu] } {
 				menu $copyMenu -tearoff 0 -type normal
-				$copyMenu add command -label "[trans emoticon_steal]-> $symbol" -command "::smiley::addSmileyFromTW {$file} {[string map {"\\" "\\\\"} $symbol]}"
+				$copyMenu add command -label "[trans emoticon_steal]-> $symbol" -command "::smiley::addSmileyFromTW [list $file] [list $symbol]"
 				$tw tag bind $twTag <Enter> "$tw configure -cursor hand2"
 				$tw tag bind $twTag <Leave> "$tw configure -cursor xterm"
 				$tw tag bind $twTag <<Button1>> "tk_popup $copyMenu %X %Y"
