4be92782310a9431dd9e94726946e052d3d19eb6
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-01-23 12:02:46 +0000

    Rewrote the plugin as it was the first plugin square87 ever made and we didnt like the code :D Still TODO: option to only work when we are HDN/FLN
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@9483 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/keepalive/keepaliveplus.tcl
+++ amsn-extras/plugins/keepalive/keepaliveplus.tcl
@@ -3,10 +3,9 @@
 	::plugins::RegisterPlugin KeepAlivePlus
 	plugins_log KeepAlivePlus "Registered plugin"
 
-	variable nickalive
-	array set ::keepalive::nickalive [list]
-
-	::plugins::RegisterEvent KeepAlivePlus user_joins_chat keepalive_plus_init
+	::plugins::RegisterEvent KeepAlivePlus chat_msg_sent message_sent
+	::plugins::RegisterEvent KeepAlivePlus chat_msg_received message_received
+	::plugins::RegisterEvent KeepAlivePlus chatwindow_closed cw_closed
 	::plugins::RegisterEvent KeepAlivePlus user_leaves_chat keepalive_plus_stop
 }
 
@@ -23,54 +22,55 @@
 		}
 	}
 
-	proc keepalive_plus {chatid} {
+	proc send_keepalive_msg {sb} {
+		set msg "MIME-Version: 1.0\r\nContent-Type: text/x-keepaliveamsn\r\n\r\n\r\n\r\n"
+		set msg_len [string bytelength $msg]
+		::MSN::WriteSBNoNL $sb "MSG" "U $msg_len\r\n$msg"
+		return
+	}
+
+	proc message_sent {event evpar} {
+		upvar 2 nick nick
+		upvar 2 msg msg
+		upvar 2 chatid chatid
+		upvar 2 win_name win_name
+		upvar 2 fontfamily fontfamily
+		upvar 2 fontstyle fontstyle
+		upvar 2 fontcolor fontcolor
+		after 50000 "::keepalive::keepalive_timer ${chatid}"
+	}
+
+	proc message_received {event evpar} {
+		upvar 2 user user
+                upvar 2 msg msg
+                upvar 2 chatid chatid
+		upvar 2 fontformat fontformat
+		upvar 2 message message
+		after 50000 "::keepalive::keepalive_timer ${chatid}"
+	}
+
+	proc keepalive_timer {chatid} {
+		after cancel "::keepalive::keepalive_timer $chatid"
 		set sb [::MSN::SBFor $chatid]
 		if {$sb == 0} {
-			after cancel "::keepalive::keepalive_plus ${chatid}"
-			unset ::keepalive::nickalive($chatid)
 			plugins_log KeepAlivePlus "YOU HAVE CLOSED THE CHAT"
-			return 0
-		}
-		set last_activity [expr [clock seconds] - [$sb cget -last_activity]]
-		if {$last_activity >47 } {
-			set chat_temp $::keepalive::nickalive($chatid)
-			set time_temp [lindex $chat_temp 0]
-			if {$time_temp < 100} {
-
-				set msg "MIME-Version: 1.0\r\nContent-Type: text/x-keepaliveamsn\r\n\r\n\r\n\r\n"
-				set msg_len [string bytelength $msg]
-				::MSN::WriteSBNoNL $sb "MSG" "U $msg_len\r\n$msg"
-
-				after 50000 "::keepalive::keepalive_plus ${chatid}"
-				unset ::keepalive::nickalive($chatid)
-				incr time_temp 1
-				set ::keepalive::nickalive($chatid) [list $time_temp]
-				set chat_temp $::keepalive::nickalive($chatid)
-				plugins_log KeepAlivePlus "ADD 1: $chatid - [lindex $chat_temp {}]"
-			} else {
-				plugins_log KeepAlivePlus "CANCEL: $chatid"
-				after cancel "::keepalive::keepalive_plus ${chatid}"
-				unset ::keepalive::nickalive($chatid)
-				return
-			}
+			return
 		} else {
-			set msg "MIME-Version: 1.0\r\nContent-Type: text/x-keepaliveamsn\r\n\r\n\r\n\r\n"
-			set msg_len [string bytelength $msg]
-			::MSN::WriteSBNoNL $sb "MSG" "U $msg_len\r\n$msg"
-
-			unset ::keepalive::nickalive($chatid)
-			set ::keepalive::nickalive($chatid) [list 0]
-			plugins_log KeepAlivePlus "ACTIVITY FOR $chatid"
-			after 50000 "::keepalive::keepalive_plus ${chatid}"
+			::keepalive::send_keepalive_msg $sb
 		}
+		after 50000 "::keepalive::keepalive_timer $chatid"
+	}
+
+	proc cw_closed {event evpar} {
+		upvar 2 chatid chatid
+		after cancel "::keepalive::keepalive_timer ${chatid}"
 	}
 
 	proc keepalive_plus_stop {event evpar} {
 		upvar 2 usr_name usr_name
 		upvar 2 chatid chatid
 		upvar 2 win_name win_name
-		after cancel "::keepalive::keepalive_plus ${chatid}"
-		unset ::keepalive::nickalive($chatid)
+		after cancel "::keepalive::keepalive_timer ${chatid}"
 	}
 
   proc deinit { } {
