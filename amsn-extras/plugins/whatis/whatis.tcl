acb88c7e250e1f421e7120c43a4bd8ee634ad1b5
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-06-16 19:55:42 +0000

    Almost works... fromLang and toLang broken
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10197 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/whatis/whatis.tcl
+++ amsn-extras/plugins/whatis/whatis.tcl
@@ -99,24 +99,24 @@ namespace eval ::whatis {
 		
 		$copymenu.whatis add cascade -label "Translate" -menu $copymenu.whatis.translate
 		menu $copymenu.whatis.translate -tearoff 0 -type normal
-		$copymenu.whatis.translate add command -label "English to Spanish" -command "whatis::translateText $w en_es"	
-		$copymenu.whatis.translate add command -label "English to French" -command "whatis::translateText $w en_fr"
-		$copymenu.whatis.translate add command -label "English to German" -command "whatis::translateText $w en_de"	
-		$copymenu.whatis.translate add command -label "English to Italian" -command "whatis::translateText $w en_it"
-		$copymenu.whatis.translate add command -label "English to Dutch" -command "whatis::translateText $w en_nl"	
-		$copymenu.whatis.translate add command -label "English to Portuguese" -command "whatis::translateText $w en_pt"
-		$copymenu.whatis.translate add command -label "English to Russian" -command "whatis::translateText $w en_ru"	
-		$copymenu.whatis.translate add command -label "English to Greek" -command "whatis::translateText $w en_el"
-		$copymenu.whatis.translate add command -label "English to Japanese"  -command "whatis::translateText $w en_ja"
-		$copymenu.whatis.translate add command -label "Dutch to English" -command "whatis::translateText $w nl_en"
-		$copymenu.whatis.translate add command -label "Spanish to English" -command "whatis::translateText $w es_en"	
-		$copymenu.whatis.translate add command -label "German to English" -command "whatis::translateText $w de_en"	
-		$copymenu.whatis.translate add command -label "Italian to English" -command "whatis::translateText $w it_en"	
-		$copymenu.whatis.translate add command -label "Portuguese to English" -command "whatis::translateText $w pt_en"	
-		$copymenu.whatis.translate add command -label "French to English" -command "whatis::translateText $w fr_en"		
-		$copymenu.whatis.translate add command -label "Russian to English" -command "whatis::translateText $w ru_en"	
-		$copymenu.whatis.translate add command -label "Greek to English" -command "whatis::translateText $w el_en"
-		$copymenu.whatis.translate add command -label "Japanese to English" -command "whatis::translateText $w ja_en"
+		$copymenu.whatis.translate add command -label "English to Spanish" -command "whatis::translateText $w en es"	
+		$copymenu.whatis.translate add command -label "English to French" -command "whatis::translateText $w en fr"
+		$copymenu.whatis.translate add command -label "English to German" -command "whatis::translateText $w en de"	
+		$copymenu.whatis.translate add command -label "English to Italian" -command "whatis::translateText $w en it"
+		$copymenu.whatis.translate add command -label "English to Dutch" -command "whatis::translateText $w en nl"	
+		$copymenu.whatis.translate add command -label "English to Portuguese" -command "whatis::translateText $w en pt"
+		$copymenu.whatis.translate add command -label "English to Russian" -command "whatis::translateText $w en ru"	
+		$copymenu.whatis.translate add command -label "English to Greek" -command "whatis::translateText $w en el"
+		$copymenu.whatis.translate add command -label "English to Japanese"  -command "whatis::translateText $w en ja"
+		$copymenu.whatis.translate add command -label "Dutch to English" -command "whatis::translateText $w nl en"
+		$copymenu.whatis.translate add command -label "Spanish to English" -command "whatis::translateText $w es en"	
+		$copymenu.whatis.translate add command -label "German to English" -command "whatis::translateText $w de en"	
+		$copymenu.whatis.translate add command -label "Italian to English" -command "whatis::translateText $w it en"	
+		$copymenu.whatis.translate add command -label "Portuguese to English" -command "whatis::translateText $w pt en"	
+		$copymenu.whatis.translate add command -label "French to English" -command "whatis::translateText $w fr en"		
+		$copymenu.whatis.translate add command -label "Russian to English" -command "whatis::translateText $w ru en"	
+		$copymenu.whatis.translate add command -label "Greek to English" -command "whatis::translateText $w el en"
+		$copymenu.whatis.translate add command -label "Japanese to English" -command "whatis::translateText $w ja en"
 	}
 	
 	#
@@ -155,61 +155,41 @@ namespace eval ::whatis {
 	#
 	# This proc will translate a text 
 	#
-	proc translateText { w transLangs} {
+	proc translateText { w fromLang toLang } {
 		set searchText [getSelectedText $w]
 		if { $searchText == "" } {
 			::amsn::messageBox "No text selected for translation!" ok error 
 		} else { 
 
-		#if { [encoding system] != "utf-8" } {
-		#	set searchText [encoding convertto utf-8 $searchText]
-		#}
-			 
-		# Translate / Get HTML file from translator
-		# Systranbox seems to stop working most of the time...
-		#if { $transLangs == "en_el" || $transLangs == "el_en" } {
-			set url "http://trans.otenet.gr/systran/box"
-			set query [::http::formatQuery id "OTEnet" lp $transLangs urltext $searchText submit.x "26" submit.y "8"]
-		#} else {
-		#	set url "http://www.systranbox.com/systran/box"
-		#	set query [::http::formatQuery systran_charset "utf-8" systran_lp $transLangs systran_text $searchText]
-		#}
+			set url "http://translate.google.com/translate_t?sl=$fromLang&tl=$toLang"
+			set query [::http::formatQuery hl "en" ie "UTF8" text $searchText sl "fromLang" tl "toLang"]
 		set http  [::http::geturl $url -query $query -timeout 72500]
 		set html  [::http::data $http]
 		
 		# Set a language title
-		switch $transLangs {
-				"en_nl" { set langTitle "Dutch" } 
-				"en_es" { set langTitle "Spanish" }		
-				"en_de" { set langTitle "German" }		
-				"en_fr" { set langTitle "French"}		
-				"en_it" { set langTitle "Italian"}		
-				"en_pt" { set langTitle "Portuguese" }		
-				"en_ru" { set langTitle "Russian" }		
-				"en_el" { set langTitle "Greek" }
-				"en_ja" { set langTitle "Japanese" }
+		switch $toLang {
+				"nl" { set langTitle "Dutch" } 
+				"es" { set langTitle "Spanish" }		
+				"de" { set langTitle "German" }		
+				"fr" { set langTitle "French"}		
+				"it" { set langTitle "Italian"}		
+				"pt" { set langTitle "Portuguese" }		
+				"ru" { set langTitle "Russian" }		
+				"el" { set langTitle "Greek" }
+				"ja" { set langTitle "Japanese" }
 				default { set langTitle "English" }
 		}
 		
 		# Strip HTML before translated text 
-		#if { $transLangs == "en_el" || $transLangs == "el_en" } {
-			set substring "td class=\"tx2\" colspan=\""
-		#} else {
-		#	set substring "name=\"translation\""
-			
-		#}
+		set substring "<div id=result_box dir=\"ltr\">"
 		set start [string first $substring $html]
 		set start [string first ">" $html $start]
 		set start [expr { $start + 1 }]
 		
 		# Stript HTML after translated text
+		puts $html
 		set htmlPart [string range $html $start end]
 		set end [expr { [string first "</" $htmlPart] - 1}]
-		#if { $transLangs == "en_el" || $transLangs == "el_en" } {
-		#	set end [expr [string first "</td>" $htmlPart] - 1]
-		#} else {
-		#	set end	[expr [string first "</textarea>" $htmlPart] - 1]
-		#}
 		set translation [string range $htmlPart 0 $end]
 		set translation [encoding convertfrom utf-8 $translation]
 				
