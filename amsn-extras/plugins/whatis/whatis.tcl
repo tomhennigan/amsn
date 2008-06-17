5a7f6dc73920d8ece134a76dc765332925d62ce7
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-06-17 12:37:06 +0000

    Now mocking firefox useragent the correct way, without bug risk
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10201 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn-extras/plugins/whatis/whatis.tcl
+++ amsn-extras/plugins/whatis/whatis.tcl
@@ -164,9 +164,7 @@ namespace eval ::whatis {
 		
 		set url "http://translate.google.com/translate_t?sl=$fromLang&tl=$toLang"
 		set query [::http::formatQuery hl "en" ie "UTF8" text $searchText sl $fromLang tl $toLang]
-		::http::config -useragent "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.14) Gecko/20080419 Ubuntu/8.04 (hardy) Firefox/2.0.0.14"
-		set http  [::http::geturl $url -query $query -timeout 72500]
-		::http::config -useragent "MSMSGS"
+		set http  [::http::geturl $url -query $query -timeout 72500 -headers {User-Agent "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.14) Gecko/20080419 Ubuntu/8.04 (hardy) Firefox/2.0.0.14"}]
 		set html  [::http::data $http]
 		
 		# Set a language title
