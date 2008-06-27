0b52501e79d48354fe557c5bcec0a8d4c3049682
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-06-27 02:55:06 +0000

    Should fix issue with users accessing spaces when not using a hotmail account! :) http://www.amsn-project.net/forums/viewtopic.php?t=5251&start=15
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10270 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/hotmail.tcl
+++ amsn/hotmail.tcl
@@ -81,7 +81,9 @@ namespace eval ::hotmail {
 		lappend vars [list ru [urlencode_full $url]]
 		lappend vars [list rru [urlencode_full $url]]
 		lappend vars [list sru [urlencode_full $url]]
-		lappend vars [list svc mail]
+		if {$id == "2" } {
+			lappend vars [list svc mail]
+		}
 		lappend vars [list js yes]
 		# The SiteID is a value that we get from the config SOAP, it's dependant on the site you want to go to.
 		# It will determine if it uses 'ru', 'rru' or 'sru' and for 'rru's it will tell the server where to go
