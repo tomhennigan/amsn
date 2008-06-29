8504af3c9059e69928b07cf281db84116a57b724
baaazen <baaazen@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-06-29 19:08:53 +0000

    oops, forgot to change the TkCxImage to webcamsn
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10287 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/webcamsn/pkgIndex.tcl
+++ amsn/utils/webcamsn/pkgIndex.tcl
@@ -2,7 +2,7 @@
 
 if {[package vcompare [info tclversion] 8.4] < 0} return
 
-if {[file exists [file join $dir [info tclversion] TkCximage[info shared]]]} {
+if {[file exists [file join $dir [info tclversion] webcamsn[info shared]]]} {
 	package ifneeded webcamsn 0.1 "package require Tk; [list load [file join $dir [info tclversion] webcamsn[info shared]] webcamsn];package provide webcamsn 0.1"
 } else {
 	package ifneeded webcamsn 0.1 "package require Tk; [list load [file join $dir webcamsn[info shared]] webcamsn];package provide webcamsn 0.1"
