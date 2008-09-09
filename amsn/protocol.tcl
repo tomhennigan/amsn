52999b95e8655f6c68d140855860076aa559d80d
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-09-09 19:07:00 +0000

    if we want to find this bug, we need to have it check -strict on $length
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10463 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/protocol.tcl
+++ amsn/protocol.tcl
@@ -4034,7 +4034,7 @@ namespace eval ::MSNOIM {
 
 				#There is a bug (#2265) where $length is not numeric
 				# report error on status_log is this occurs so we can track it down
-				if {![string is integer $length]} {
+				if {![string is integer -strict $length]} {
 					status_log "#-----------------------#\nERROR in $self :: var \$length is \"$length\" while it should be an integer !!!\n\tCommand: $command (last element is length)\n#-----------------------#" white
 				}
 
