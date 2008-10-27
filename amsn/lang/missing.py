33ce37aef25bc382a1992cbcd2860c49907e3512
billiob <billiob@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-10-27 16:31:15 +0000

    fix indentation
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10648 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/lang/missing.py
+++ amsn/lang/missing.py
@@ -20,51 +20,51 @@ CYAN=36
 WHITE=37
 
 def esc(text,color,bold=False):
-	if nocolors: return text
-	seq="\33["+str(color)
-	if bold: seq=seq+";1"
-	seq=seq+"m"
-	return seq+text+"\33[0m"
+    if nocolors: return text
+    seq="\33["+str(color)
+    if bold: seq=seq+";1"
+    seq=seq+"m"
+    return seq+text+"\33[0m"
 
 def usage():
-	print "Usage: missing.py [--nocolor] [--hidetrans] langXX"
+    print "Usage: missing.py [--nocolor] [--hidetrans] langXX"
 
 #By default, colors are shown
 nocolors=False
 hidetrans=False
-	
+    
 msg_list = {}
 
 #Get options
 try:
-	opts,args=getopt.getopt(sys.argv[1:],"n",["nocolor","hidetrans"])
+    opts,args=getopt.getopt(sys.argv[1:],"n",["nocolor","hidetrans"])
 except getopt.GetoptError:
-	usage()
-	sys.exit(-1)
-	
+    usage()
+    sys.exit(-1)
+    
 #Process options
 for opt,arg in opts:
-	if opt in ("--nocolor"): nocolors=True
-	elif opt in ("--hidetrans"): hidetrans=True
-	
+    if opt in ("--nocolor"): nocolors=True
+    elif opt in ("--hidetrans"): hidetrans=True
+    
 #After parsing options, we should just have the language file
 if len(args) != 1:
-	usage()
-	sys.exit(-1)
+    usage()
+    sys.exit(-1)
 
 #Try to open the master file
 try:
-	langen=open("langen")
+    langen=open("langen")
 except IOError:
-	print "'langen' master keys file is missing"
-	sys.exit(1)
-#Try to open the language file to check	
+    print "'langen' master keys file is missing"
+    sys.exit(1)
+#Try to open the language file to check 
 try: 
     f=open(args[0])
 except IOError:
     print "Couldn't open lang file '"+args[0]+"'"
     sys.exit(2)
-	
+    
 ###################################
 #Processing of the master keys
 print
@@ -87,7 +87,7 @@ for num,line in enumerate(langen):
     if tabs>0:
         print esc(" ERROR:",RED)+esc(" found TAB \\t character in 'langen'",BOLD)
         print " -->",esc("Line "+str(num+2),YELLOW,True)
-	continue
+        continue
     
     if i < 0:
         print esc(" ERROR:",RED)+esc(" invalid key in 'langen', you should remove it",BOLD)
@@ -107,8 +107,8 @@ loaded_keys=[]
 #Skin version line
 line=f.next().strip()
 if not line==header:
-        print esc(" ERROR:",RED)+esc(" Header in file should be "+header,BOLD)
-	print " --> ",esc(line,YELLOW,True)
+    print esc(" ERROR:",RED)+esc(" Header in file should be "+header,BOLD)
+    print " --> ",esc(line,YELLOW,True)
 for num,line in enumerate(f):
     
     line=line.strip()
@@ -124,8 +124,8 @@ for num,line in enumerate(f):
     if tabs>0:
         print esc(" ERROR:",RED)+esc(" found TAB \\t character",BOLD)
         print " -->",esc("Line "+str(num+2),YELLOW,True)
-	continue	
-	
+        continue    
+    
     if i < 0:
         print esc(" ERROR:",RED)+esc(" invalid key, you should remove it",BOLD)
         print " Line "+str(num+2)+" -->",esc(line.strip(),YELLOW,True)
@@ -142,19 +142,19 @@ for num,line in enumerate(f):
         del msg_list[key]
     except KeyError:
         print esc(" warning:",YELLOW)+esc(" found possibly deprecated key",BOLD)
-	print " Line "+str(num+2)+" --> '"+esc(key,YELLOW,True)+"'. Please remove it if it's not used in latest AMSN stable version"
+        print " Line "+str(num+2)+" --> '"+esc(key,YELLOW,True)+"'. Please remove it if it's not used in latest AMSN stable version"
 
 ###################################
 #Print results
 num_missing=len(msg_list.items())
 if num_missing>0:
-	errormsg=str(num_missing)+" missing keys in "+args[0]+":"
-	print
-	print esc(errormsg,YELLOW,True)
-	print "-"*len(errormsg)
+    errormsg=str(num_missing)+" missing keys in "+args[0]+":"
+    print
+    print esc(errormsg,YELLOW,True)
+    print "-"*len(errormsg)
 else:
-	print
-	print esc("** No missing keys in "+args[0]+" **",GREEN)
+    print
+    print esc("** No missing keys in "+args[0]+" **",GREEN)
 
 #Print sorted missing keys
 keys=msg_list.keys()
@@ -162,6 +162,6 @@ keys.sort()
 for key in keys:
     if hidetrans: print esc(key,CYAN,True)
     else:
-    	print esc(key,CYAN,True)+": "+msg_list[key]
+        print esc(key,CYAN,True)+": "+msg_list[key]
 
 print
