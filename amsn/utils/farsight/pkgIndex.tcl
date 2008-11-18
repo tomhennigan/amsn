5a1a0b0329a417ceeca4965c00f4db27a44dca39
tomhennigan <tomhennigan@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-11-18 02:15:19 +0000

    Farsight+Gstreamer intel dylibs! Finally! Not working "out of the box" just yet, but we'll get there. See: http://amsn-project.net/wiki/Farsight#Mac
    - DYLD_LIBRARY_PATH issue not resolved, it's 2am so I'll come back to it tomorrow =)
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10734 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/farsight/pkgIndex.tcl
+++ amsn/utils/farsight/pkgIndex.tcl
@@ -2,4 +2,11 @@
 
 if {[package vcompare [info tclversion] 8.4] < 0} return
 
-package ifneeded Farsight 0.1 "[list load [file join $dir tcl_farsight[info shared]] Farsight];package provide Farsight 0.1"
+# TEMP! fix for ppc macs while they aren't supported will be removed soon.
+if { [OnMac] } {
+    if { $::tcl_platform(byteOrder) == "bigEndian" } {
+        set dir [file join $dir "disabled_on_ppc"]
+    }
+}
+
+package ifneeded Farsight 0.1 "[list load [file join $dir tcl_farsight[info shared]] Farsight]; package provide Farsight 0.1"
