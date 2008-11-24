803b7d3e833498ad45de4cfeb3cf089140adcce8
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2008-11-24 19:01:51 +0000

    replace the button2label trick for mac by a snit object in order to allow a configure -command on the labels.
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@10759 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/amsncore.tcl
+++ amsn/amsncore.tcl
@@ -329,37 +329,6 @@ proc ::tk::TextKeySelect {w new} {
 }
 
 #///////////////////////////////////////////////////////////////////////////////
-# if a button has a -image, -relief flat but not -overrelief, it will actually be created as a label
-# this is a workaround for platforms like macos and tileqt which have a problem with buttons (like
-# not honouring "-relief flat" (tileqt) or not supporting alpha transparancy(macos))
-# TODO: add a bind that works as -command on a button (mousebutton press, move away, release does not trigger)
-proc buttons2labels { } {
-	if { [info commands ::tk::button2] == "" } { rename button ::tk::button2 }
-	proc button { pathName args } {
-		array set options $args
-		if { [info exists options(-image)] && [info exists options(-relief)] && $options(-relief) == "flat" } {
-			if { [info exists options(-command)] } {
-				set command $options(-command)
-				unset options(-command)
-			}
-			if { [info exists options(-overrelief)] } { unset options(-overrelief) }
-			set ret [eval label [list $pathName] [array get options]]
-			if { [info exists command] } {
-				bind $pathName <<Button1>> "$command"
-			}
-		} else {
-			set ret [eval ::tk::button2 [list $pathName] $args]
-		}
-		return $ret
-	}
-}
-# apply buttons2labels on Mac, because there seem to be problems with buttons there
-# TODO: as soon as it is fixed in tk on mac, make it version-conditional
-if { [OnMac] } {
-	buttons2labels
-}
-
-#///////////////////////////////////////////////////////////////////////////////
 # highlight_selected_tags (text, tags)
 # This proc will go through the text widget 'text' add an extra tag to any characters that are
 # selected and have a certain tag. This is used to highlight coloured text.
