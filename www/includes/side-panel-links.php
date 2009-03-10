b10d0ee0229bc79fd5da4e37b78fcbbcbef6b49a
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-03-10 17:05:43 +0000

    Updating sourceforge logo as requested by the new sourceforge policies
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11085 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/includes/side-panel-links.php
+++ www/includes/side-panel-links.php
@@ -6,7 +6,7 @@
 <br/><br/>
 <a href="http://sourceforge.net/projects/amsn/"><?php echo trans('sfprojectpagelink') ?></a>
 <br/><br/>
-<a href="http://sourceforge.net/"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=54091&amp;type=1" alt="SourceForge.net Logo" /></a>
+<a href="http://sourceforge.net/projects/amsn"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=54091&type=12" width="120" height="30" border="0" alt="Get aMSN at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
 <br/><br/>
 <a href="http://validator.w3.org/check?uri=referer"><img src="images/vxhtml.png" alt="Valid XHTML 1.0!" /></a>
 <br/><br/>
