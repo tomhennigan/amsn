82118cc3b3311b7e18a41eade22f9c5b8c1f9648
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-06-11 23:47:05 +0000

    Adding ColoredNicks as default activated plugin as per request : http://www.amsn-project.net/forums/viewtopic.php?t=6666
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11231 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/plugins.tcl
+++ amsn/plugins.tcl
@@ -1300,7 +1300,7 @@ namespace eval ::plugins {
 			#logic: if this is the first time the user logged in, then 
 			#the configuration list is empty which means load_config
 			#makes loadedplugins empty
-			set loadedplugins [list "Nudge" "Winks" "Cam Shooter" "remind" "Notes"]
+			set loadedplugins [list "Nudge" "Winks" "Cam Shooter" "remind" "Notes" "ColoredNicks"]
 
 			status_log "Plugins System: load_config: No plugins.xml]\n" red
 		}
