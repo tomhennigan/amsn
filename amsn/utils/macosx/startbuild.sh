99f136f1e6322945a8923c01e82197168069f2af
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-07-22 05:32:04 +0000

    Updating a bit startbuild.sh script
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11363 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/startbuild.sh
+++ amsn/utils/macosx/startbuild.sh
@@ -1,4 +1,5 @@
-export CFLAGS='-arch ppc -arch i386 -iwithsysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3'
+echo "Make sure you run this with '. ./utils/macosx/startbuild.sh' (start with a dot then space then path) so that the exported variables affect your current shell"
+export CFLAGS='-arch ppc -arch i386 -iwithsysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.3'
 export CXXFLAGS=$CFLAGS
 export LDFLAGS=$CFLAGS
 export MACOSX_DEPLOYMENT_TARGET=10.3
