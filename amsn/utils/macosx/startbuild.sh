87e72a70ccec0ebac5cfdacbb11077be734753a8
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-07-22 02:14:55 +0000

    Adding a script to set up the environment for building on mac
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11358 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn/utils/macosx/startbuild.sh
@@ -0,0 +1,4 @@
+export CFLAGS='-arch ppc -arch i386 -iwithsysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3'
+export CXXFLAGS=$CFLAGS
+export LDFLAGS=$CFLAGS
+export MACOSX_DEPLOYMENT_TARGET=10.3
