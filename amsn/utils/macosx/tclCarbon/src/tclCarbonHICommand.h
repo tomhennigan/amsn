070a759b042dfc7342aa596fc74c0fb7339b7a8a
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-07-23 16:49:08 +0000

    Fix compilation issue with tclCarbon and reenable compilation of the extension and update the binary to be using stubs
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11379 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/tclCarbon/src/tclCarbonHICommand.h
+++ amsn/utils/macosx/tclCarbon/src/tclCarbonHICommand.h
@@ -1,7 +1,25 @@
 #ifndef __TCLCARBONHICOMMAND_H_INCLUDE
 #define __TCLCARBONHICOMMAND_H_INCLUDE
 
+
+#ifdef __APPLE__
+#define Cursor       QD_Cursor
+#define WindowPtr    QD_WindowPtr
+#define Picture      QD_Picture
+#define BOOL         OSX_BOOL
+#define EventType    HIT_EventType
+#endif
+
 #include <Carbon/Carbon.h>
+
+#ifdef __APPLE__
+#undef Cursor
+#undef WindowPtr
+#undef Picture
+#undef BOOL
+#undef EventType
+#endif
+
 #include "tk.h"
 #include "tcl.h"
 
--- amsn/utils/macosx/tclCarbon/src/tclCarbonNotification.h
+++ amsn/utils/macosx/tclCarbon/src/tclCarbonNotification.h
@@ -1,7 +1,24 @@
 #ifndef __TCLCARBONNOTIFICATION_H_INCLUDE
 #define __TCLCARBONNOTIFICATION_H_INCLUDE
 
+#ifdef __APPLE__
+#define Cursor       QD_Cursor
+#define WindowPtr    QD_WindowPtr
+#define Picture      QD_Picture
+#define BOOL         OSX_BOOL
+#define EventType    HIT_EventType
+#endif
+
 #include <Carbon/Carbon.h>
+
+#ifdef __APPLE__
+#undef Cursor
+#undef WindowPtr
+#undef Picture
+#undef BOOL
+#undef EventType
+#endif
+
 #include "tk.h"
 #include "tcl.h"
 
