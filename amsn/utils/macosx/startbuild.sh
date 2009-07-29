547119c0837d9be95534169046f833f1e7307d4a
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-07-29 19:01:00 +0000

    Fix the CFLAGS to have binaries built for ppc and not ppc7400 using the 10.4 sdk instead of 10.5
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11414 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/startbuild.sh
+++ amsn/utils/macosx/startbuild.sh
@@ -1,5 +1,5 @@
 echo "Make sure you run this with '. ./utils/macosx/startbuild.sh' (start with a dot then space then path) so that the exported variables affect your current shell"
-export CFLAGS='-arch ppc -arch i386 -iwithsysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.3'
+export CFLAGS='-arch ppc -arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3'
 export CXXFLAGS=$CFLAGS
 export LDFLAGS="$CFLAGS -headerpad_max_install_names"
 export MACOSX_DEPLOYMENT_TARGET=10.3
