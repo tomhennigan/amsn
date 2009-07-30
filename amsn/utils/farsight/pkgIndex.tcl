0ecdb141e8b5fa11696f3d92a21f54e4d0014f09
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-07-30 19:43:28 +0000

    haha, that was the problem! Tom! it's your fault... now ppc farsight should work too
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11425 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/farsight/pkgIndex.tcl
+++ amsn/utils/farsight/pkgIndex.tcl
@@ -2,11 +2,4 @@
 
 if {[package vcompare [info tclversion] 8.4] < 0} return
 
-# TEMP! fix for ppc macs while they aren't supported will be removed soon.
-if { ![catch {tk windowingsystem} wsystem] && $wsystem == "aqua" } {
-    if { $::tcl_platform(byteOrder) == "bigEndian" } {
-        set dir [file join $dir "disabled_on_ppc"]
-    }
-}
-
 package ifneeded Farsight 0.1 "[list load [file join $dir tcl_farsight[info shared]] Farsight]; package provide Farsight 0.1"
