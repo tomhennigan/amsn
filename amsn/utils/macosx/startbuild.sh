d0249f7e460247459fe9e5bae2612d302eb9e108
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-09-19 09:54:11 +0000

    on Mac OS X 10.6, the compiler only compiled 64bits and MUST be used with the 10.6 SDK.. if we want to use 10.4 or 10.5 SDKs or build 32bits binaries, we must set CC and CXX to gcc-4.0 and g++-4.0
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11613 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/macosx/startbuild.sh
+++ amsn/utils/macosx/startbuild.sh
@@ -1,4 +1,6 @@
 echo "Make sure you run this with '. ./utils/macosx/startbuild.sh' (start with a dot then space then path) so that the exported variables affect your current shell"
+export CC="gcc-4.0"
+export CXX="g++-4.0"
 export CFLAGS='-arch ppc -arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3'
 export CXXFLAGS=$CFLAGS
 export LDFLAGS="$CFLAGS -headerpad_max_install_names"
