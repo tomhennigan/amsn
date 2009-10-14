f05a5f8636ad77ec4033d4e8cf295d47d85a9afd
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-10-14 20:43:54 +0000

    No more need for this Vista hack (assuming r11698 fixes it, which it seems it does on my Win7)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11700 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/config.tcl
+++ amsn/config.tcl
@@ -1657,7 +1657,7 @@ proc CheckLock { email } {
 	set Port [LoginList getlock 0 $email]
 	status_log "CheckLock: LoginList getlock called. Lock=$Port\n" blue
 	if { $Port != 0 } {
-		if { [OnWinVista] || [catch {socket -server phony $Port} newlockSock] != 0 } {
+		if { [catch {socket -server phony $Port} newlockSock] != 0 } {
 			#status_log "CheckLock Port is already in use: $newlockSock\n" red
 			# port is taken, let's make sure it's a profile lock
 			foreach {local_host} [list localhost [info hostname] 127.0.0.1] {
