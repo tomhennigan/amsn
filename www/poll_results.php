ce17a1082afc27ab77c270d339f28e7835157caf
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-11-04 22:06:49 +0000

    Fix poll_results to correctly use the translations
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11746 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/poll_results.php
+++ www/poll_results.php
@@ -9,8 +9,6 @@
 
 $id = (isset($_GET['poll']) && ereg('^[0-9]+$', $_GET['poll'])) ? (int)$_GET['poll'] : 0;
 
-$trans_qu='poll_question';
-$trans_ans='poll_answer';
 $query_poll = mysql_query("SELECT `id`, question FROM `amsn_poll` " . ($id === 0 ? 'ORDER BY id DESC' : "WHERE `id` = '{$id}'"));
 
 if (!mysql_num_rows($query_poll)) {
@@ -19,7 +17,6 @@ if (!mysql_num_rows($query_poll)) {
 }
 
 while ($poll = mysql_fetch_row($query_poll)) {
-$idq=$poll[0];
 	$total_query = mysql_query("SELECT SUM(`votes`) AS total, MAX(`votes`) AS max FROM `amsn_poll_answers` WHERE `id_father` = '" . (int)$poll[0] . "'");
 	
 	if (mysql_num_rows($total_query) ) {
@@ -36,11 +33,10 @@ $idq=$poll[0];
 	$query_answers = mysql_query("SELECT id, answer, `votes` FROM `amsn_poll_answers` WHERE `id_father` = '" . (int)$poll[0] . "' ORDER BY id");
 	
 	if (mysql_num_rows($query_answers) > 1) {
-	echo '<h3>'.trans($idq, $trans_qu)."</h3>\n<ul>\n";
+	echo '<h3>'.trans($poll[0], 'poll_question', $poll[1])."</h3>\n<ul>\n";
 	while ($row = mysql_fetch_row($query_answers)) {
-		$ida=$row[0];
 		$percentage =  (int) (100 * $row[2] / $total);
-		echo '<li>'.trans($ida, $trans_ans).' ('.POLLS_VOTES.': '.$row[2].' '.(($total === 0) ? "" : " - <b>".$percentage.'%</b>)');
+		echo '<li>'.trans($row[0], 'poll_answer', $row[1]).' ('.POLLS_VOTES.': '.$row[2].' '.(($total === 0) ? "" : " - <b>".$percentage.'%</b>)');
 		echo '<br/><img alt="bar chart" src="images/poll_graph.php?percent='.$percentage.'&amp;val='.(int)($percentage * 100 / $max).'"/></li>';
 	}
 	echo "</ul>\n";
