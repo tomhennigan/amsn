50b14f22436c90be9bcdacc79fb1f3c4e852d08a
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-11-07 23:56:20 +0000

    Adding Maemo download section (WIP)
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11761 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ www/maemo-downloads.php
@@ -0,0 +1,20 @@
+<?php
+   define('source', 'download');
+   include 'common.php';
+   include inc . 'init.php';
+
+   echo '<link rel="stylesheet" type="text/css" media="screen" href="download.css" />';
+   
+   include inc . 'header.php';
+?>
+
+<br>
+<img style="display: block;margin-left: auto;margin-right: auto;" src="images/download-maemo.png" alt="Maemo Download" ><br>
+	<a name="INSTALL"></a>
+	<h3><?=SCREENSHOTS;?></h3>
+	<dl>
+	<dt>
+	</dt>
+        </dl>
+    <br>
+<?php include inc . 'footer.php'; ?>
