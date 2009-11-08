20f5b775b2eed4656bb125235cc2d5fb13e04ae3
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-11-08 08:23:33 +0000

    Add a language sorting algorithm to have it sorted by 'most commonly used' rather than alphabetical
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11770 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/includes/side-panel-language.php
+++ www/includes/side-panel-language.php
@@ -4,10 +4,38 @@
 <div class="block_right_content block_language">
 <?php
 $flag=0;
+$languages = array();
+
+function cmp_langs($a, $b)
+{
+    $values = array('en' => 0, 'fr' => 1, 'es' => 2, 'de' => 3, 'tr' => 4);
+    if (isset($values[$a])) {
+        $av = $values[$a];
+    } else {
+        $av = 10000;
+    }
+
+    if (isset($values[$b])) {
+        $bv = $values[$b];
+    } else {
+        $bv = 10000;
+    }
+    if ($av == $bv) {
+        return 0;
+    }
+    return ($av < $bv) ? -1 : 1;
+}
+
 foreach (glob('includes/languages/*/*.php') as $filename) {
   $keywords = preg_split("/\//", $filename);
   $lang = $keywords[2];
 
+  $languages[] = $lang;
+}
+
+usort($languages, "cmp_langs");
+
+foreach ($languages as $lang) {
   /* Does flag for language exist? Hide if not as there is no pic */
   if(is_readable('images/flags/'.$lang.'.png')) {
     echo '<a href="?lang='.$lang.'"><img src="images/flags/'.$lang.'.png" alt="'.$lang.'" /></a> ';
