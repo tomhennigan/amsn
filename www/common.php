8daa9a59d395098e54baf97006b106abf6c403aa
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-11-09 16:24:45 +0000

    oups, need to include english as default for lang.php stuff too
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11776 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- www/common.php
+++ www/common.php
@@ -13,9 +13,9 @@ mysql_select_db(DBNAME_WWW, mysql_connect(DBHOST,DBUSER,DBPASS)) or die(mysql_er
 
 header('Content-type: text/html;charset=utf-8');
 
+include("includes/languages/en/en.php");  
 if (file_exists('includes/languages/'.$lang_set.'/'.$lang_set.'.php')) {
   include("includes/languages/".$lang_set."/".$lang_set.".php");  
-} else {
-  include("includes/languages/en/en.php");  
 }
+
 ?>
