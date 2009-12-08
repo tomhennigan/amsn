df6a09b032ffb8f3b1ccc13ef274363e8879e357
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-12-08 22:18:58 +0000

    get the nick/psm of the offline contacts only after we set our own intial status, otherwise, because of huge network bottleneck, it looks like sometimes, it could happen that the getprofile we sent initially just gets lost and the answer never reaches us... result, no  nick change, no ILNs..
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11835 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/protocol.tcl
+++ amsn/protocol.tcl
@@ -4951,6 +4951,10 @@ namespace eval ::MSNOIM {
 				::MSN::changeName [::abook::getPersonal MFN] 1
 			}
 
+			foreach username [::MSN::getList FL] {
+				after idle [list catch [list $::roaming GetProfile [list ::MSN::roaming_cl_get_profile_cb $username] $username]]
+			}
+
 			# Change status after sending the UUX stuff
 			#ChCustomState $newstate_custom
 			#send_dock "STATUS" $newstate
@@ -7185,12 +7189,6 @@ proc ::MSN::ABSynchronizationDone { initial error } {
 			ns setInitialStatus
 		}
 		ns authenticationDone
-
-		if {$initial } {
-			foreach username [::MSN::getList FL] {
-				after idle [list catch [list $::roaming GetProfile [list ::MSN::roaming_cl_get_profile_cb $username] $username]]
-			}
-		}
 	
 	} elseif {$error == 2 } {
 		#ABDoesNotExist
