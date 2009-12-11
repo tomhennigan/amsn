407913cd5fd603be5caf9a3889fb93d895033d23
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2009-12-11 23:05:24 +0000

    Okay, now www. has to be at the start of a word. If you type a www.. you're looking for trouble :p
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11845 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/gui.tcl
+++ amsn/gui.tcl
@@ -331,7 +331,7 @@ if { $initialize_amsn == 1 } {
 		#this regexp is a bit complex, but it reaches all URLs as specified in the RFC 1738 on http://www.ietf.org/rfc/rfc1738.txt
 		set urlregexps {
 			{\w+://[\%\/\$\*\~\|\,\!\'\#\.\@\+\-\=\?\;\:\^\&\_[:alnum:]]+}
-			{www\.[\%\/\$\*\~\,\|\!\'\#\.\@\+\-\=\?\;\:\^\&\_[:alnum:]]+}
+			{\mwww\.[\%\/\$\*\~\,\|\!\'\#\.\@\+\-\=\?\;\:\^\&\_[:alnum:]]+}
 			{(?:[\%\/\$\*\~\,\!\'\|\#\@\+\-\=\?\;\:\^\&\_[:alnum:]]+\.)+(?:org|com|net)(?:/[\%\/\$\*\~\,\!\'\|\#\.\@\+\-\=\?\;\:\^\&\_[:alnum:]]*)*(?=\y)}
 			{spotify:(?:track|album|artist|search|playlist|user|radio):[^<>\s]+}
 		}
