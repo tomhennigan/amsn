bd3f0cd8ade9a6f5e4dd7fa896d6103c12bfafdb
kakaroto <kakaroto@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2010-03-07 20:28:16 +0000

    finish the script for auto creating tarballs
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@11982 f248a3e1-9e13-0410-b2cf-a4429de6d898

--- amsn/utils/make_amsn_release.sh
+++ amsn/utils/make_amsn_release.sh
@@ -19,7 +19,7 @@ svn export https://amsn.svn.sourceforge.net/svnroot/amsn/tags/$TAG $DIR_NAME >  
 
 # Export plugins
 echo "Exporting plugins to $DIR_NAME/plugins"
-svn export https://amsn.svn.sourceforge.net/svnroot/amsn/trunk/amsn-extras/plugins/ $DIR_NAME/plugins  >  /dev/null
+svn export --force https://amsn.svn.sourceforge.net/svnroot/amsn/trunk/amsn-extras/plugins/ $DIR_NAME/plugins  >  /dev/null
 
 #delete unwanted plugins
 for plugin in $RM_PLUGINS
@@ -44,13 +44,43 @@ tar -cjf amsn-$VERSION-full.tar.bz2 $DIR_NAME
 
 # Create source tarballs
 echo "Creating source tarballs"
-echo "NOT YET SUPPORTED"
-
-# Create windows  tarballs
-echo "Creating windows tarballs"
-echo "NOT YET SUPPORTED"
+cd $DIR_NAME
+rm -rf utils/windows/gnash utils/windows/gstreamer utils/windows/reg1.1 utils/windows/snack2.2 utils/windows/tkdnd/ utils/windows/tkvideo1.3.0/ utils/windows/winico0.6
+find . -name "*.dll" | xargs rm -rf
+find . -name "*.exe" | xargs rm -rf
+rm -rf utils/macosx/sndplay utils/macosx/cabextract utils/macosx/QuickTimeTcl3.1 utils/macosx/gstreamer/ utils/macosx/tclAE2.0.4 utils/macosx/tls1.5.0
+find . -name "*.dylib" | xargs rm -rf
+find . -name "*.so" | xargs rm -rf
+cd $RELEASE_DIR
+tar -czf amsn-$VERSION-src.tar.gz $DIR_NAME
+tar -cjf amsn-$VERSION-src.tar.bz2 $DIR_NAME
+rm -rf $DIR_NAME
+tar -xjf amsn-$VERSION-full.tar.bz2
 
 # Create mac tarballs
 echo "Creating mac tarballs"
-echo "NOT YET SUPPORTED"
+cd $DIR_NAME
+find . -name "Rules.mk" | xargs rm -rf
+rm -rf utils/windows/ utils/linux/ autopackage/ debian/
+find . -name "*.dll" | xargs rm -rf
+find . -name "*.exe" | xargs rm -rf
+find . -name "*.dsp" | xargs rm -rf
+find . -name "*.dsw" | xargs rm -rf
+find . -name "*.vcproj" | xargs rm -rf
+find . -name "*.sln" | xargs rm -rf
+find . -name "*.c" | xargs rm -rf
+find . -name "*.cpp" | xargs rm -rf
+find . -name "*.h" | xargs rm -rf
+rm -rf utils/macosx/tclCarbon/src utils/macosx/aMSN.xcodeproj utils/macosx/statusicon/*.[mh] utils/macosx/sndplay-src utils/macosx/snack2.2/patches utils/macosx/make_dmg utils/macosx/macDock/src utils/macosx/growl1.0/src utils/macosx/*.sh
+rm -rf utils/TkCximage/demos/ utils/TkCximage/src/ utils/asyncresolver/src/ utils/farsight/src/ utils/gupnp utils/libaio utils/tclISF/src utils/tcl_siren/src/ utils/webcamsn/src
+cd $RELEASE_DIR
+tar -czf amsn-$VERSION-mac.tar.gz $DIR_NAME
+tar -cjf amsn-$VERSION-mac.tar.bz2 $DIR_NAME
+rm -rf $DIR_NAME
+tar -xjf amsn-$VERSION-full.tar.bz2
+
+
+# Create windows tarballs
+echo "Creating windows tarballs"
+echo "** NSIS should take only the needed files, no need for this then"
 
