f053a9336c0fd92dd4ba9d5cb4463fc25e1e7819
vivia <vivia@f248a3e1-9e13-0410-b2cf-a4429de6d898>
2010-09-18 22:50:17 +0000

    Added a plugin to ignore the PSM forced by eBuddy and use the cached one instead
    git-svn-id: svn://svn.code.sf.net/p/amsn/code/trunk@12185 f248a3e1-9e13-0410-b2cf-a4429de6d898

---
+++ amsn-extras/plugins/ebuddykiller/ebuddykiller.tcl
@@ -0,0 +1,47 @@
+namespace eval ::ebuddykiller {
+	variable loaded
+
+	proc Init { dir } {
+		variable loaded
+
+		if {[info exists loaded] && $loaded == 1} {
+			return
+		}
+
+		if { [info commands ::NS::Snit_methodsetInitialNicknameCB] != "" && [info commands ::NS::Snit_methodsetInitialNicknameKilled] == "" } {
+			rename ::NS::Snit_methodsetInitialNicknameCB ::NS::Snit_methodsetInitialNicknameKilled
+		} else {
+			return
+		}
+
+		::plugins::RegisterPlugin eBuddyKiller
+		plugins_log "eBuddyKiller" "We are being executed"
+
+		proc ::NS::Snit_methodsetInitialNicknameCB { type selfns win self newstate newstate_custom nickname last_modif psm fail } {
+			plugins_log "eBuddyKiller" "Callback called"
+			if { [string first " - on eBuddy Mobile Messenger http://get.ebuddy.com" $psm] >= 0 } {
+				plugins_log "eBuddyKiller" "eBuddy PSM killed!!!"
+				set psm [::abook::getPersonal PSM]
+			}
+			eval ::NS::Snit_methodsetInitialNicknameKilled [list $type $selfns $win $self $newstate $newstate_custom $nickname $last_modif $psm $fail]
+		}
+
+		set loaded 1
+	}
+
+
+	proc DeInit { } {
+		variable loaded
+		
+		status_log "Restoring previous proc body\n"
+	
+		;# this will return  the  list of arguments a command can take
+		if { [info commands ::NS::Snit_methodsetInitialNicknameKilled] != "" } {
+			rename ::NS::Snit_methodsetInitialNicknameCB ""
+			rename ::NS::Snit_methodsetInitialNicknameKilled ::NS::Snit_methodsetInitialNicknameCB
+		}
+	
+		set loaded 0
+		
+	}
+}
